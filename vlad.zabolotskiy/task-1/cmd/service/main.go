cbbc045ed64d38d86223d7a9ab0eacee7b338c23
Vladislav Zabolotskiy <se1lzor@MacBook-Air-Vladislav.local>
2025-09-17 23:24:30 +0300

    [TASK-1] Fix main.go for test again

--- vlad.zabolotskiy/task-1/cmd/service/main.go
+++ vlad.zabolotskiy/task-1/cmd/service/main.go
@@ -14,45 +14,45 @@ func main() {
 	input, _ := reader.ReadString('\n')
 	parts := strings.Fields(input)
 	if len(parts) > 1 {
-		fmt.Println("Enter exactly one numbers!")
+		fmt.Println("Enter exactly one numbers")
 		return
 	}
 	if len(parts) == 0 {
-		fmt.Println("Empty string!")
+		fmt.Println("Empty string")
 		return
 	}
 
 	a, err1 := strconv.ParseInt(parts[0], 10, 64)
 	if err1 != nil {
-		fmt.Println("Invalid first operand!")
+		fmt.Println("Invalid first operand")
 		return
 	}
 
 	input2, _ := reader.ReadString('\n')
 	parts2 := strings.Fields(input2)
 	if len(parts2) > 1 {
-		fmt.Println("Enter exactly one numbers!")
+		fmt.Println("Enter exactly one numbers")
 		return
 	}
 	if len(parts2) == 0 {
-		fmt.Println("Empty string!")
+		fmt.Println("Empty string")
 		return
 	}
 
 	b, err2 := strconv.ParseInt(parts2[0], 10, 64)
 	if err2 != nil {
-		fmt.Println("Invalid second operand!")
+		fmt.Println("Invalid second operand")
 		return
 	}
 
 	opInput, _ := reader.ReadString('\n')
 	sign := strings.TrimSpace(opInput)
 	if len(sign) > 1 {
-		fmt.Println("Enter exactly one operation!")
+		fmt.Println("Enter exactly one operation")
 		return
 	}
 	if len(sign) == 0 {
-		fmt.Println("Empty string!")
+		fmt.Println("Empty string")
 		return
 	}
 
@@ -78,18 +78,18 @@ func calculate(n1, n2 int64, s string) (string, error) {
 		result = fmt.Sprintf("%d", n1*n2)
 	case "/":
 		if n2 == 0 {
-			err = fmt.Errorf("divisor by zero")
+			err = fmt.Errorf("Divisor by zero")
 		} else {
 			result = fmt.Sprintf("%d", n1/n2)
 		}
 	case ":":
 		if n2 == 0 {
-			err = fmt.Errorf("divisor by zero")
+			err = fmt.Errorf("Divisor by zero")
 		} else {
 			result = fmt.Sprintf("%d", n1/n2)
 		}
 	default:
-		err = fmt.Errorf("invalid operation")
+		err = fmt.Errorf("Invalid operation")
 	}
 
 	return result, err
