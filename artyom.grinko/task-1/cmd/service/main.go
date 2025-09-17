e6b4ba4fbd22a7ccfb1f1daa7c74c995003d4c19
Timothée <sainetsauf@disroot.org>
2025-09-17 18:35:44 +0300

    [TASK-1] Imagine using tabs instead of spaces in 2025.

--- artyom.grinko/task-1/cmd/service/main.go
+++ artyom.grinko/task-1/cmd/service/main.go
@@ -8,47 +8,47 @@ import (
 )
 
 func die(message string) {
-    fmt.Fprintln(os.Stderr, message)
+	fmt.Fprintln(os.Stderr, message)
 
-    os.Exit(1)
+	os.Exit(1)
 }
 
 func read(scanner *bufio.Scanner) string {
-    scanner.Scan()
-    if scanner.Err() != nil {
-        die("Quel dommage")
-    }
+	scanner.Scan()
+	if scanner.Err() != nil {
+		die("Quel dommage")
+	}
 
-    return scanner.Text()
+	return scanner.Text()
 }
 
-var operations = map[string]func(int64, int64) int64 {
-    "+": func(x, y int64) int64 { return x + y },
-    "-": func(x, y int64) int64 { return x - y },
-    "*": func(x, y int64) int64 { return x * y },
-    "/": func(x, y int64) int64 { return x / y },
+var operations = map[string]func(int64, int64) int64{
+	"+": func(x, y int64) int64 { return x + y },
+	"-": func(x, y int64) int64 { return x - y },
+	"*": func(x, y int64) int64 { return x * y },
+	"/": func(x, y int64) int64 { return x / y },
 }
 
 func main() {
-    stdin := bufio.NewScanner(os.Stdin)
-    x, err := strconv.ParseInt(read(stdin), 10, 0)
-    if err != nil {
-        die("Invalid first operand")
-    }
+	stdin := bufio.NewScanner(os.Stdin)
+	x, err := strconv.ParseInt(read(stdin), 10, 0)
+	if err != nil {
+		die("Invalid first operand")
+	}
 
-    y, err := strconv.ParseInt(read(stdin), 10, 0)
-    if err != nil {
-        die("Invalid first operand")
-    }
+	y, err := strconv.ParseInt(read(stdin), 10, 0)
+	if err != nil {
+		die("Invalid first operand")
+	}
 
-    operation := read(stdin)
-    if y == 0 && operation == "/" {
-        die("Division by zero")
-    }
-    
-    if f, ok := operations[operation]; ok {
-        fmt.Println(f(x, y))
-    } else {
-        die("Invalid operation")
-    }
+	operation := read(stdin)
+	if y == 0 && operation == "/" {
+		die("Division by zero")
+	}
+
+	if f, ok := operations[operation]; ok {
+		fmt.Println(f(x, y))
+	} else {
+		die("Invalid operation")
+	}
 }
