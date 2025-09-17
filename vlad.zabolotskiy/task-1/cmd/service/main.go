193be13ab10ac49996ab9003a8eee53001b0f3a7
Vladislav Zabolotskiy <se1lzor@MacBook-Air-Vladislav.local>
2025-09-17 22:10:49 +0300

    [TASK-1] Initial commit

---
+++ vlad.zabolotskiy/task-1/cmd/service/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Enter first operand:")
+	input, _ := reader.ReadString('\n')
+
+	parts := strings.Fields(input)
+	if len(parts) > 1 {
+		fmt.Println("Enter exactly one numbers!")
+		return
+	}
+	if len(parts) == 0 {
+		fmt.Println("Empty string!")
+		return
+	}
+
+	a, err1 := strconv.ParseInt(parts[0], 10, 64)
+	if err1 != nil {
+		fmt.Println("Invalid first operand!")
+		return
+	}
+
+	fmt.Println("Enter second operand:")
+	input2, _ := reader.ReadString('\n')
+
+	parts2 := strings.Fields(input2)
+	if len(parts2) > 1 {
+		fmt.Println("Enter exactly one numbers!")
+		return
+	}
+	if len(parts2) == 0 {
+		fmt.Println("Empty string!")
+		return
+	}
+
+	b, err2 := strconv.ParseInt(parts2[0], 10, 64)
+	if err2 != nil {
+		fmt.Println("Invalid second operand!")
+		return
+	}
+
+	fmt.Println("Select operation ('+', '-', '*', ':'): ")
+	opInput, _ := reader.ReadString('\n')
+	sign := strings.TrimSpace(opInput)
+	if len(sign) > 1 {
+		fmt.Println("Enter exactly one operation!")
+		return
+	}
+	if len(sign) == 0 {
+		fmt.Println("Empty string!")
+		return
+	}
+
+	result, err := calculate(a, b, sign)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Result:", result)
+}
+
+func calculate(n1, n2 int64, s string) (string, error) {
+	var result string
+	var err error
+
+	switch s {
+	case "+":
+		result = fmt.Sprintf("%d", n1+n2)
+
+	case "-":
+		result = fmt.Sprintf("%d", n1-n2)
+
+	case "*":
+		result = fmt.Sprintf("%d", n1*n2)
+
+	case "/":
+		if n2 == 0 {
+			err = fmt.Errorf("Divisor by zero!")
+		} else {
+			result = fmt.Sprintf("%d", n1/n2)
+		}
+
+	default:
+		err = fmt.Errorf("Invalid operation!")
+	}
+
+	return result, err
+}
---
+++ vlad.zabolotskiy/task-1/go.mod
@@ -0,0 +1,5 @@
+//Module Name
+module github.com/se1lzor/lets-go-programming-v2025-autumn-spbstu-40007
+
+//GO version
+go 1.22.7
