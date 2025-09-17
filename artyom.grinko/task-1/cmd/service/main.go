0908ac48248b99d6d683d589b84ad479bbf20595
Timothée <sainetsauf@disroot.org>
2025-09-17 18:25:46 +0300

    [TASK-1] Initial commit.

---
+++ artyom.grinko/task-1/cmd/service/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func die(message string) {
+    fmt.Fprintln(os.Stderr, message)
+
+    os.Exit(1)
+}
+
+func read(scanner *bufio.Scanner) string {
+    scanner.Scan()
+    if scanner.Err() != nil {
+        die("Quel dommage")
+    }
+
+    return scanner.Text()
+}
+
+var operations = map[string]func(int64, int64) int64 {
+    "+": func(x, y int64) int64 { return x + y },
+    "-": func(x, y int64) int64 { return x - y },
+    "*": func(x, y int64) int64 { return x * y },
+    "/": func(x, y int64) int64 { return x / y },
+}
+
+func main() {
+    stdin := bufio.NewScanner(os.Stdin)
+    x, err := strconv.ParseInt(read(stdin), 10, 0)
+    if err != nil {
+        die("Invalid first operand")
+    }
+
+    y, err := strconv.ParseInt(read(stdin), 10, 0)
+    if err != nil {
+        die("Invalid first operand")
+    }
+
+    operation := read(stdin)
+    if y == 0 && operation == "/" {
+        die("Division by zero")
+    }
+    
+    if f, ok := operations[operation]; ok {
+        fmt.Println(f(x, y))
+    } else {
+        die("Invalid operation")
+    }
+}
