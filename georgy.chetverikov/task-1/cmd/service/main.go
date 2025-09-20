a7df2da789947d0c0144c3fe7a681cabb053b340
Georgy Chetverikov <blessedcakee@outlook.com>
2025-09-20 22:56:06 +0300

    [TASK-1] third try

--- georgy.chetverikov/task-1/cmd/service/main.go
+++ georgy.chetverikov/task-1/cmd/service/main.go
@@ -1,75 +1,72 @@
-
 package main
 
 import (
-	"fmt"
 	"errors"
+	"fmt"
 	"log"
 )
 
 func substract(x int, y int) int {
-	return x - y;	
+	return x - y
 }
 
 func summarize(x int, y int) int {
-	return x + y;
+	return x + y
 }
 
 func multiply(x int, y int) int {
-	return x * y;
+	return x * y
 }
 
 func divide(x int, y int) (int, error) {
 	if y == 0 {
-		return 0, errors.New("division by zero");
+		return 0, errors.New("division by zero")
 	}
 
-	return x/y, nil;
+	return x / y, nil
 }
 
 func main() {
 
-	fmt.Println("Please, enter some\"x\" number and some \"y\" number");
-	
-	var x,y int;
+	fmt.Println("Please, enter some\"x\" number and some \"y\" number")
 
-	_, err1 := fmt.Scanln(&x);
+	var x, y int
+
+	_, err1 := fmt.Scanln(&x)
 
 	if err1 != nil {
-		log.Fatal(errors.New("invalid fisrt operand"));
+		log.Fatal(errors.New("invalid fisrt operand"))
 	}
 
-	_, err2 := fmt.Scanln(&y);
+	_, err2 := fmt.Scanln(&y)
 	if err2 != nil {
-		log.Fatal(errors.New("invalid second operand"));
+		log.Fatal(errors.New("invalid second operand"))
 	}
 
-	fmt.Println("Now choose the operation you want to do. \"-\" - substract, \"+\" - summarize, \"*\" - multiply, \"/\" - divide");
+	fmt.Println("Now choose the operation you want to do. \"-\" - substract, \"+\" - summarize, \"*\" - multiply, \"/\" - divide")
 
-	var option string;
+	var option string
 
-	_, err3 := fmt.Scanln(&option);
+	_, err3 := fmt.Scanln(&option)
 	if err3 != nil {
-		log.Fatal(errors.New("invalid operation"));
+		log.Fatal(errors.New("invalid operation"))
 	}
 
 	switch option {
 	case "-":
-		fmt.Println(substract(x,y));
+		fmt.Println(substract(x, y))
 	case "+":
-		fmt.Println(summarize(x,y));
+		fmt.Println(summarize(x, y))
 	case "*":
-		fmt.Println(multiply(x,y));
+		fmt.Println(multiply(x, y))
 	case "/":
-		result, err := divide(x,y);
-		if  err != nil {
-			fmt.Println("Error: ", err);
+		result, err := divide(x, y)
+		if err != nil {
+			fmt.Println("Error: ", err)
 		} else {
-			fmt.Println(result);
+			fmt.Println(result)
 		}
 	default:
-		log.Fatal(errors.New("invalid operation"));	
+		log.Fatal(errors.New("invalid operation"))
 	}
 }
-
-
