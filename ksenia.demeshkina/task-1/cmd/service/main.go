4d88c3f9b5e8e05d61a602bb434811db48d701a8
Ксения Демешкина <demeshkinakseniaa@gmail.com>
2025-09-21 20:41:03 +0300

    [TASK-1] fix errors

--- ksenia.demeshkina/task-1/cmd/service/main.go
+++ ksenia.demeshkina/task-1/cmd/service/main.go
@@ -30,16 +30,19 @@ func main() {
 	_, err := fmt.Fscan(os.Stdin, &number1)
 	if err != nil {
 		fmt.Println("Invalid first operand")
+		return
 	}
 
 	_, err = fmt.Fscan(os.Stdin, &number2)
 	if err != nil {
 		fmt.Println("Invalid second operand")
+		return
 	}
 
 	_, err = fmt.Fscan(os.Stdin, &symbol)
 	if err != nil {
 		fmt.Println("Invalid operation")
+		return
 	}
 
 	if symbol != "+" && symbol != "-" && symbol != "/" && symbol != "*" {
