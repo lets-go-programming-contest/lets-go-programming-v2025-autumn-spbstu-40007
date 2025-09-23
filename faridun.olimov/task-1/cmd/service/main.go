300b9a2febfa5f8e2659f6398b308fcd5cdb0e14
Олимов Фаридун <maska192yt@mail.ru>
2025-09-23 16:54:05 +0300

    [TASK-1] Initial commit

---
+++ faridun.olimov/task-1/cmd/service/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+func main() {
+	var first, second int
+	var operation string
+
+	_, err := fmt.Scanln(&first)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scanln(&second)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err = fmt.Scanln(&operation)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	switch operation {
+	case "+":
+		fmt.Println(first + second)
+	case "-":
+		fmt.Println(first - second)
+	case "*":
+		fmt.Println(first * second)
+	case "/":
+		if second == 0 {
+			fmt.Println("Division by zero")
+		} else {
+			fmt.Println(first / second)
+		}
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
