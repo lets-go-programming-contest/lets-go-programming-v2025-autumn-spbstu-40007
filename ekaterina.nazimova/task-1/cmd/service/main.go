57eebf10c451dcacaf01af4f1e4a28de5bb21d0b
Nazimova Ekaterina <ekaterinanaimova07@gmail.com>
2025-09-25 15:40:57 +0300

    [TASK-1] add main code

--- ekaterina.nazimova/task-1/cmd/service/main.go
+++ ekaterina.nazimova/task-1/cmd/service/main.go
@@ -5,10 +5,15 @@ import (
 )
 
 func main() {
+<<<<<<< HEAD
 	var (
 		num1, num2 int
 		op         string
 	)
+=======
+	var num1, num2 int
+	var op string
+>>>>>>> 9e86d2a ([TASK-1] add main code)
 
 	if _, err := fmt.Scanln(&num1); err != nil {
 		fmt.Println("Invalid first operand")
@@ -20,6 +25,7 @@ func main() {
 		return
 	}
 
+<<<<<<< HEAD
 	if _, err := fmt.Scanln(&op); err != nil {
 		fmt.Println("Invalid operation")
 		return
@@ -33,12 +39,27 @@ func main() {
 	case "*":
 		fmt.Println(num1 * num2)
 	case "/":
+=======
+	fmt.Scanln(&op)
+
+	if op == "+" {
+		fmt.Println(num1 + num2)
+	} else if op == "-" {
+		fmt.Println(num1 - num2)
+	} else if op == "*" {
+		fmt.Println(num1 * num2)
+	} else if op == "/" {
+>>>>>>> 9e86d2a ([TASK-1] add main code)
 		if num2 == 0 {
 			fmt.Println("Division by zero")
 		} else {
 			fmt.Println(num1 / num2)
 		}
+<<<<<<< HEAD
 	default:
+=======
+	} else {
+>>>>>>> 9e86d2a ([TASK-1] add main code)
 		fmt.Println("Invalid operation")
 	}
 }
