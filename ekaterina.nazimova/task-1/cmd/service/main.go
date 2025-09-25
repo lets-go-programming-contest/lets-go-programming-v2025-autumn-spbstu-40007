9e86d2aa037f9b9faa32bafc22330b164dba3a72
Nazimova Ekaterina <ekaterinanaimova07@gmail.com>
2025-09-25 15:40:57 +0300

    [TASK-1] add main code

--- ekaterina.nazimova/task-1/cmd/service/main.go
+++ ekaterina.nazimova/task-1/cmd/service/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var num1, num2 int
+	var op string
+
+	if _, err := fmt.Scanln(&num1); err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	if _, err := fmt.Scanln(&num2); err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	fmt.Scanln(&op)
+
+	if op == "+" {
+		fmt.Println(num1 + num2)
+	} else if op == "-" {
+		fmt.Println(num1 - num2)
+	} else if op == "*" {
+		fmt.Println(num1 * num2)
+	} else if op == "/" {
+		if num2 == 0 {
+			fmt.Println("Division by zero")
+		} else {
+			fmt.Println(num1 / num2)
+		}
+	} else {
+		fmt.Println("Invalid operation")
+	}
 }
