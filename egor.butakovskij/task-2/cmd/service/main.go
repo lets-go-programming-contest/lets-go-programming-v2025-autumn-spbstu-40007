9018d0c0ffe6974b3d0c1a7c67bfcb7e1fe6d955
egor butakovskij <mrtntkatz@gmail.com>
2025-09-30 23:44:59 +0300

    [TASK-2-1] Initial commit

---
+++ egor.butakovskij/task-2/cmd/service/main.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+func getRecomendedTemperature(K int) {
+	var (
+		temp, recTemp, highBorder, lowBorder int
+		sign                                 string
+	)
+	recTemp = 15
+	highBorder = 30
+	lowBorder = 15
+	for j := 0; j < K; j++ {
+		_, err := fmt.Scanf("%s %d", &sign, &temp)
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+
+		if temp < lowBorder && sign == "<=" || temp > highBorder && sign == ">=" {
+			fmt.Println("-1")
+			return
+		}
+		if sign == ">=" && temp <= highBorder && temp >= lowBorder {
+			recTemp = temp
+			lowBorder = temp
+		}
+
+		if sign == "<=" && temp <= highBorder && temp >= lowBorder {
+			highBorder = temp
+		}
+
+		if sign == "<=" && recTemp > temp {
+			fmt.Println("-1")
+			return
+		}
+
+		fmt.Println(recTemp)
+	}
+}
+
+func main() {
+	var (
+		N, K int
+	)
+
+	_, err := fmt.Scan(&N)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	for i := 0; i < N; i++ {
+		_, err := fmt.Scan(&K)
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+		getRecomendedTemperature(K)
+	}
+}
