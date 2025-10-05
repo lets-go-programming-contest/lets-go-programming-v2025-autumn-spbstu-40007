23ca5d655e8cbb4de8dd125809e785b0c551d9f5
seeany1 <seeany1@icloud.com>
2025-10-05 19:33:01 +0300

    [TASK-2-1]

--- oksuz.enes/task-2-1/cmd/service/main.go
+++ oksuz.enes/task-2-1/cmd/service/main.go
@@ -8,31 +8,43 @@ import (
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	var room, workers int
-	if _, err := fmt.Fscan(reader, &room, &workers); err != nil {
+	var departments, workers int
+	if _, err := fmt.Fscan(reader, &departments); err != nil {
 		return
 	}
-	minTemp := 15
-	maxTemp := 30
-	for range workers {
-		var operator string
-		var temperature int
-		if _, err := fmt.Fscan(reader, &operator, &temperature); err != nil {
+
+	for range departments {
+		if _, err := fmt.Fscan(reader, &workers); err != nil {
 			return
 		}
-		if operator == ">=" {
-			if temperature > minTemp {
-				minTemp = temperature
+
+		minTemp := 15
+		maxTemp := 30
+
+		for range workers {
+			var (
+				operator    string
+				temperature int
+			)
+			if _, err := fmt.Fscan(reader, &operator, &temperature); err != nil {
+				return
 			}
-		} else if operator == "<=" {
-			if temperature < maxTemp {
-				maxTemp = temperature
+
+			if operator == ">=" {
+				if temperature > minTemp {
+					minTemp = temperature
+				}
+			} else if operator == "<=" {
+				if temperature < maxTemp {
+					maxTemp = temperature
+				}
 			}
-		}
-		if minTemp > maxTemp {
-			fmt.Println(-1)
-		} else {
-			fmt.Println(minTemp)
+
+			if minTemp > maxTemp {
+				fmt.Println(-1)
+			} else {
+				fmt.Println(minTemp)
+			}
 		}
 	}
 }
