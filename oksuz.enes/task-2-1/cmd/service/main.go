f3e8eb2845a6e0cd774dc85f638b8fe6f7f4e086
seeany1 <seeany1@icloud.com>
2025-10-05 19:28:00 +0300

    [TASK-2-1]

--- oksuz.enes/task-2-1/cmd/service/main.go
+++ oksuz.enes/task-2-1/cmd/service/main.go
@@ -8,37 +8,31 @@ import (
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-
 	var room, workers int
 	if _, err := fmt.Fscan(reader, &room, &workers); err != nil {
 		return
 	}
-
 	minTemp := 15
 	maxTemp := 30
-
 	for range workers {
 		var operator string
-		var temp int
-
-		if _, err := fmt.Fscan(reader, &operator, &temp); err != nil {
+		var temperature int
+		if _, err := fmt.Fscan(reader, &operator, &temperature); err != nil {
 			return
 		}
-
 		if operator == ">=" {
-			if temp > minTemp {
-				minTemp = temp
+			if temperature > minTemp {
+				minTemp = temperature
 			}
 		} else if operator == "<=" {
-			if temp < maxTemp {
-				maxTemp = temp
+			if temperature < maxTemp {
+				maxTemp = temperature
 			}
 		}
-	}
-
-	if minTemp > maxTemp {
-		fmt.Println(-1)
-	} else {
-		fmt.Println(minTemp)
+		if minTemp > maxTemp {
+			fmt.Println(-1)
+		} else {
+			fmt.Println(minTemp)
+		}
 	}
 }
