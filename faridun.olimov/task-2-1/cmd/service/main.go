ae94b8f2f3998569f857e2e512e337efe1f72e14
Олимов Фаридун <maska192yt@mail.ru>
2025-10-06 17:00:16 +0300

    [TASK-2-1] Initial commit

---
+++ faridun.olimov/task-2-1/cmd/service/main.go
@@ -0,0 +1,79 @@
+package main
+
+import "fmt"
+
+func intMax(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
+}
+
+func intMin(x, y int) int {
+	if x < y {
+		return x
+	}
+
+	return y
+}
+
+func processDepartment(numRequests int) {
+	var (
+		lowBorder       = 15
+		highBorder      = 30
+		inContradiction bool
+	)
+
+	for range numRequests {
+		var (
+			temperature int
+			sign        string
+		)
+
+		_, err := fmt.Scanf("%s %d", &sign, &temperature)
+		if err != nil {
+			return
+		}
+
+		if inContradiction {
+			fmt.Println("-1")
+
+			continue
+		}
+
+		if sign == ">=" {
+			lowBorder = intMax(lowBorder, temperature)
+		} else if sign == "<=" {
+			highBorder = intMin(highBorder, temperature)
+		}
+
+		if lowBorder > highBorder {
+			inContradiction = true
+			fmt.Println("-1")
+		} else {
+			fmt.Println(lowBorder)
+		}
+	}
+}
+
+func main() {
+	var (
+		numDepartments int
+		numRequests    int
+	)
+
+	_, err := fmt.Scan(&numDepartments)
+	if err != nil {
+		return
+	}
+
+	for range numDepartments {
+		_, err = fmt.Scan(&numRequests)
+		if err != nil {
+			return
+		}
+
+		processDepartment(numRequests)
+	}
+}
