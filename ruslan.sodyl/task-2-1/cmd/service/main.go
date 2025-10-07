098ac334561744e0856edfe6c474cc5ce2f57c26
tyvyt2 <skhmail.ru@gmail.com>
2025-10-08 02:20:21 +0300

    Update main.go

--- ruslan.sodyl/task-2-1/cmd/service/main.go
+++ ruslan.sodyl/task-2-1/cmd/service/main.go
@@ -4,46 +4,49 @@ import "fmt"
 
 func process(emploeesNumb uint16) {
 	var (
-		temp 	uint8
-		sign 	string
-		minTemp uint8 = 15 
+		temp    uint8
+		sign    string
+		minTemp uint8 = 15
 		maxTemp uint8 = 30
-		
 	)
 	const (
 		minConstTemp = 15
 		maxConstTemp = 30
-		moreSign = ">="
-		lessSign = "<="
+		moreSign     = ">="
+		lessSign     = "<="
 	)
 
 	for range emploeesNumb {
-		_, err := fmt.Scan(&sign,&temp)
-		if err != nil || sign != lessSign && sign != moreSign || 
-		temp > maxConstTemp || temp < minConstTemp {
+		_, err := fmt.Scan(&sign, &temp)
+		if err != nil || sign != lessSign && sign != moreSign ||
+			temp > maxConstTemp || temp < minConstTemp {
 			fmt.Println("Invalid temperature")
-			continue 
+			continue
 		}
 
 		switch sign {
 		case ">=":
-			if minTemp < temp {minTemp = temp}
+			if minTemp < temp {
+				minTemp = temp
+			}
 		case "<=":
-			if maxTemp > temp {maxTemp = temp}
+			if maxTemp > temp {
+				maxTemp = temp
+			}
 		}
 
 		if minTemp <= maxTemp {
 			fmt.Println(minTemp)
-		}else{
+		} else {
 			fmt.Println(-1)
 		}
 	}
 }
 func main() {
-	var(
-		departNumb,emploeesNumb uint16
+	var (
+		departNumb, emploeesNumb uint16
 	)
-	
+
 	_, err := fmt.Scan(&departNumb)
 	if err != nil || departNumb > 1000 || departNumb < 1 {
 		fmt.Println("Invalid number of departments")
