46d28f0b7ea468f50ff34f60940329b8c44ddfb9
Amirkhan Myasoutov <>
2025-10-07 18:34:54 +0300

    [TASK-2-2] Sketch

--- amirkhan.myasoutov/task-2-2/cmd/service/main.go
+++ amirkhan.myasoutov/task-2-2/cmd/service/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var (
+		N    int
+		mass []int
+	)
+
+	_, err := fmt.Scanln(&N)
+	if err != nil || N < 0 || N > 10000 {
+		fmt.Println("Invalid number of dishes")
+		return
+	}
+
+	for i := 0; i < N; i++ {
+		var ai int
+		_, err := fmt.Scanln(&ai)
+		if err != nil || N < -10000 || N > 10000 {
+			fmt.Println("Invalid")
+			return
+		}
+		mass = append(mass, ai)
+	}
+
+	for i := 0; i < len(mass); i++ {
+		fmt.Println(mass[i])
+	}
 }
