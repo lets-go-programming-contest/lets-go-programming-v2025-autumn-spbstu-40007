4981557201d5f3ca221d5e17e88fb2561f97e7b2
tyvyt2 <skhmail.ru@gmail.com>
2025-10-08 01:41:51 +0300

    Update main.go

--- ruslan.sodyl/task-2-1/cmd/service/main.go
+++ ruslan.sodyl/task-2-1/cmd/service/main.go
@@ -1,10 +1,11 @@
 package main
 import ("fmt")
 
-func main(){
+func process(K uint16){
 	var(
-		N,K uint16
-		temp, minTemp, maxTemp uint8
+		temp uint8
+		minTemp uint8 = 15 
+		maxTemp uint8 = 30
 		sign string
 	)
 	const(
@@ -13,39 +14,43 @@ func main(){
 		moreSign = ">="
 		lessSign = "<="
 	)
-  
+	
+	for range K {
+		_, err := fmt.Scan(&sign,&temp)
+		if err != nil || sign != lessSign && sign != moreSign || 
+		temp > maxConstTemp || temp < minConstTemp {
+			fmt.Println("Invalid temperature")
+			return
+		}
+		switch sign {
+		case ">=":
+			if minTemp < temp {minTemp = temp}
+		case "<=":
+			if maxTemp > temp {maxTemp = temp}
+		}
+		if minTemp <= maxTemp{
+			fmt.Println(minTemp)
+		}else{
+			fmt.Println(-1)
+		}
+	}
+}
+func main(){
+	var(
+		N,K uint16
+	)
+	
 	_, err := fmt.Scan(&N)
 	if err != nil || N > 1000 || N < 1{
 		fmt.Println("Invalid number of departmens")
 		return
 	}
-
 	for range N {
-		minTemp = 15
-		maxTemp = 30
 		_, err = fmt.Scan(&K)
 		if err != nil || K > 1000 || K < 1{
 			fmt.Println("Invalid number of emploees")
 			return
 		}
-		for range K {
-			_, err = fmt.Scan(&sign,&temp)
-			if err != nil || sign != lessSign && sign != moreSign || 
-			temp > maxConstTemp || temp < minConstTemp {
-				fmt.Println("Invalid temperature")
-				return
-			}
-			switch sign {
-			case ">=":
-				if minTemp < temp {minTemp = temp}
-			case "<=":
-				if maxTemp > temp {maxTemp = temp}
-			}
-			if minTemp <= maxTemp{
-				fmt.Println(minTemp)
-			}else{
-				fmt.Println(-1)
-			}
-		}
+		process(K)
 	}
 }
