d8a66a3bb0d41b0b746d2acc33c7f35ea115356f
Amirkhan Myasoutov <>
2025-10-07 21:18:39 +0300

    [TASK-2-1] Add main code

--- amirkhan.myasoutov/task-2-1/cmd/service/main.go
+++ amirkhan.myasoutov/task-2-1/cmd/service/main.go
@@ -1,5 +1,39 @@
 package main
 
+import "fmt"
+
 func main() {
+	var departmentAmount int
+	fmt.Scan(&departmentAmount)
+
+	for i := 0; i < departmentAmount; i++ {
+		maximumTemp := 30
+		minimumTemp := 15
+
+		var employeeAmount int
+		fmt.Scan(&employeeAmount)
+
+		for j := 0; j < employeeAmount; j++ {
+			var (
+				operand     string
+				temperature int
+			)
+
+			fmt.Scan(&operand, &temperature)
+
+			if operand == "<=" && temperature < maximumTemp {
+				maximumTemp = temperature
+			}
+			if operand == ">=" && temperature > minimumTemp {
+				minimumTemp = temperature
+			}
+
+			if minimumTemp <= maximumTemp {
+				fmt.Println(minimumTemp)
+			} else {
+				fmt.Println(-1)
+			}
+		}
+	}
 
 }
