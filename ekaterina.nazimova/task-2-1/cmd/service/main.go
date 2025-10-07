f6b1d311a384e6d875ae8b313ef8cfba3175627f
UwUshkin <ekaterinanazimova07@gmail.com>
2025-10-07 18:53:37 +0300

    [TASK-2-1] Add main code

--- ekaterina.nazimova/task-2-1/cmd/service/main.go
+++ ekaterina.nazimova/task-2-1/cmd/service/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	var n int
+	fmt.Scan(&n)
+
+	for i := 0; i < n; i++ {
+		var k int
+		fmt.Scan(&k)
+
+		minTemp := 15
+		maxTemp := 30
+
+		for j := 0; j < k; j++ {
+			var op string
+			var x int
+			fmt.Scan(&op, &x)
+
+			if op == ">=" && x > minTemp {
+				minTemp = x
+			}
+			if op == "<=" && x < maxTemp {
+				maxTemp = x
+			}
+
+			if minTemp <= maxTemp {
+				fmt.Println(minTemp)
+			} else {
+				fmt.Println(-1)
+			}
+		}
+	}
 }
