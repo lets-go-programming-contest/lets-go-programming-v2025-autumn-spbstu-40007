8676d1e1d5ca9e3904f99a826bdeaa6555e940f4
rom1424 <5356488@gmail.com>
2025-10-09 00:20:10 +0300

    [TASK-1] update main

--- obraztsov.roman/task-1/cmd/service/main.go
+++ obraztsov.roman/task-1/cmd/service/main.go
@@ -3,43 +3,43 @@ package main
 import "fmt"
 
 func main() {
-    var (
-        first, second int16
-        operation     string
-    )
+	var (
+		first, second int16
+		operation     string
+	)
 
-    _, err1 := fmt.Scanln(&first)
-    if err1 != nil {
-        fmt.Println("Invalid first operand")
-        return
-    }
+	_, err1 := fmt.Scanln(&first)
+	if err1 != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
 
-    _, err2 := fmt.Scanln(&second)
-    if err2 != nil {
-        fmt.Println("Invalid second operand")
-        return
-    }
+	_, err2 := fmt.Scanln(&second)
+	if err2 != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
 
-    _, err3 := fmt.Scanln(&operation)
-    if err3 != nil {
-        fmt.Println("Invalid operation")
-        return
-    }
+	_, err3 := fmt.Scanln(&operation)
+	if err3 != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
 
-    switch operation {
-    case "+":
-        fmt.Println(first + second)
-    case "-":
-        fmt.Println(first - second)
-    case "*":
-        fmt.Println(first * second)
-    case "/":
-        if second == 0 {
-            fmt.Println("Division by zero")
-            return
-        }
-        fmt.Println(first / second)
-    default:
-        fmt.Println("Invalid operation")
-    }
+	switch operation {
+	case "+":
+		fmt.Println(first + second)
+	case "-":
+		fmt.Println(first - second)
+	case "*":
+		fmt.Println(first * second)
+	case "/":
+		if second == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(first / second)
+	default:
+		fmt.Println("Invalid operation")
+	}
 }
