1cdfe3b85d64d003e84b60ae29994186d776302b
UwUshkin <ekaterinanazimova07@gmail.com>
2025-10-08 21:08:27 +0300

    [TASK-2-2] Fix lint 15

--- ekaterina.nazimova/task-2-2/cmd/service/main.go
+++ ekaterina.nazimova/task-2-2/cmd/service/main.go
@@ -27,6 +27,7 @@ func (h *IntHeap) Pop() interface{} {
 func main() {
 	var dishAmount int
 	_, err := fmt.Scan(&dishAmount)
+
 	if err != nil {
 		fmt.Println(err)
 
@@ -39,6 +40,7 @@ func main() {
 	for range make([]int, dishAmount) {
 		var dish int
 		_, err = fmt.Scan(&dish)
+
 		if err != nil {
 			fmt.Println(err)
 
@@ -50,6 +52,7 @@ func main() {
 
 	var dishNumber int
 	_, err = fmt.Scan(&dishNumber)
+
 	if err != nil {
 		fmt.Println(err)
 
@@ -60,6 +63,7 @@ func main() {
 
 	for range make([]int, dishNumber) {
 		val, ok := heap.Pop(dishHeap).(int)
+
 		if !ok {
 			fmt.Println("Unexpected type")
 
