4c339f66d3ef4bd825b95d465e5c6b04448acf6f
tyvyt2 <skhmail.ru@gmail.com>
2025-10-08 04:24:28 +0300

    [TASK-2-2] Initial commit

---
+++ ruslan.sodyl/task-2-2/cmd/service/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+	"sort"
+)
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func process(dishes uint16) {
+	var (
+		numb int
+		key  uint16
+	)
+
+	h := &IntHeap{}
+	heap.Init(h)
+	for range dishes {
+		_, err := fmt.Scan(&numb)
+		if err != nil {
+			fmt.Println("Invalid number")
+
+			continue
+		}
+
+		heap.Push(h, numb)
+	}
+
+	_, err := fmt.Scan(&key)
+	if err != nil || key < 1 || key > 10000 {
+		fmt.Println("Invalid k-number")
+
+		return
+	}
+
+	sort.Slice(*h, func(i, j int) bool {
+		return (*h)[i] < (*h)[j]
+	})
+	
+	key = dishes - key
+	fmt.Println((*h)[key])
+}
+
+func main() {
+	var dishes uint16
+
+	_, err := fmt.Scan(&dishes)
+	if err != nil || dishes < 1 || dishes > 10000 {
+		fmt.Println("Invalid number of dishes")
+
+		return
+	}
+
+	process(dishes)
+}
