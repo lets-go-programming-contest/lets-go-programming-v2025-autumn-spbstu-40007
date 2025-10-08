4be1efd7300298834848cb88a15d6950ad622434
rom1424 <5356488@gmail.com>
2025-10-08 12:09:35 +0300

    [TASK-1] Initial commit

---
+++ obraztsov.roman/task-1/cmd/service/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		first, second int16
+		operation     string
+	)
+
+	// Чтение первого числа
+	_, err1 := fmt.Scanln(&first) // Используем Scan вместо Scanln
+	if err1 != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	// Чтение второго числа
+	_, err2 := fmt.Scanln(&second) // Используем Scan вместо Scanln
+	if err2 != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	// Чтение операции
+	_, err3 := fmt.Scanln(&operation) // Используем Scan вместо Scanln
+	if err3 != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	// Обработка операций
+	switch operation {
+	case "+":
+		fmt.Println(first + second)
+	case "-":
+		fmt.Println(first - second)
+	case "*":
+		fmt.Println(first * second)
+	case "/":
+		if second == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(first / second)
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
