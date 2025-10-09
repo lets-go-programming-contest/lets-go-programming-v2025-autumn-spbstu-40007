080324f09b401175e27ab0ce80fdeb86882ed421
Bokova Daria <bokovad2@gmail.com>
2025-10-09 23:57:44 +0300

    [TASK-2-2] Initial commit

---
+++ daria.bokova/task-2-2/cmd/service/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type MaxHeap struct {
+	elements []int
+}
+
+func (h MaxHeap) Len() int { return len(h.elements) }
+
+func (h MaxHeap) Less(i, j int) bool {
+	return h.elements[i] > h.elements[j]
+}
+
+func (h MaxHeap) Swap(i, j int) {
+	h.elements[i], h.elements[j] = h.elements[j], h.elements[i]
+}
+
+func (h *MaxHeap) Push(x interface{}) {
+	h.elements = append(h.elements, x.(int))
+}
+
+func (h *MaxHeap) Pop() interface{} {
+	old := h.elements
+	n := len(old)
+	item := old[n-1]
+	h.elements = old[0 : n-1]
+	return item
+}
+
+func (h *MaxHeap) Peek() int {
+	return h.elements[0]
+}
+
+func main() {
+	var totalNumbers, targetPosition int
+
+	if _, scanErr := fmt.Scan(&totalNumbers); scanErr != nil {
+		return
+	}
+
+	numberSequence := make([]int, totalNumbers)
+	for idx := 0; idx < totalNumbers; idx++ {
+		if _, scanErr := fmt.Scan(&numberSequence[idx]); scanErr != nil {
+			return
+		}
+	}
+
+	if _, scanErr := fmt.Scan(&targetPosition); scanErr != nil {
+		return
+	}
+
+	numHeap := &MaxHeap{}
+	heap.Init(numHeap)
+
+	for _, value := range numberSequence {
+		heap.Push(numHeap, value)
+	}
+
+	for extractCount := 1; extractCount < targetPosition; extractCount++ {
+		heap.Pop(numHeap)
+	}
+
+	fmt.Println(numHeap.Peek())
+}
