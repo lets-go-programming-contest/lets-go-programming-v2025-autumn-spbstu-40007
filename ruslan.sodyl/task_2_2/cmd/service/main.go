a8692cfca0eefdd3ff5c86758dd34e87eb3f9d4a
tyvyt2 <skhmail.ru@gmail.com>
2025-10-12 13:21:45 +0300

    [TASK-2-2] Initial commit

---
+++ ruslan.sodyl/task_2_2/cmd/service/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+	"sort"
+)
+
+type IntHeap []int
+
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
+func (h *IntHeap) Push(x any) {
+	if val, ok := x.(int); ok {
+		*h = append(*h, val)
+	}
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+
+	return x
+}
+
+func process(dishes uint16) {
+	var (
+		numb int
+		key  uint16
+	)
+
+	kucha := &IntHeap{}
+	heap.Init(kucha)
+
+	for range dishes {
+		if _, err := fmt.Scan(&numb); err != nil {
+			fmt.Println("Invalid number")
+
+			continue
+		}
+
+		heap.Push(kucha, numb)
+	}
+
+	if _, err := fmt.Scan(&key); err != nil || key < 1 || key > 10000 {
+		fmt.Println("Invalid k-number")
+
+		return
+	}
+
+	sort.Slice(*kucha, func(i, j int) bool { return (*kucha)[i] < (*kucha)[j] })
+
+	key = dishes - key
+	fmt.Println((*kucha)[key])
+}
+
+func main() {
+	var dishes uint16
+
+	_, err := fmt.Scan(&dishes)
+	if err != nil || dishes < 1 || dishes > 10000 {
+		fmt.Println("Invalid number of dishes")
+
+		return
+	}
+
+	process(dishes)
+}
+
+//go run task_2_2/cmd/service/main.go
---
+++ ruslan.sodyl/task_2_2/go.mod
@@ -0,0 +1,3 @@
+module github.com/tyvyt2/task-2-1
+
+go 1.22.7
