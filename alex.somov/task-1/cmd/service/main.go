4c2ca3ee1366a4cdd0f702d55afc188a6522067d
AlexSomov1 <76060083+AlexSomov1@users.noreply.github.com>
2025-10-12 17:15:42 +0300

    [TASK-1] (#45)
    * [TASK-1] Initial commit
    
    * [TASK-1] Fix structure
    
    * [TASK-1] Fix error text
    
    * [TASK-1] Fix lint
    
    ---------
    
    Co-authored-by: AlexSomov <allexx@MacBook-Air-AlexSomov.local>

---
+++ alex.somov/task-1/cmd/service/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+)
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func subtract(a int, b int) int {
+	return a - b
+}
+
+func multiply(a int, b int) int {
+	return a * b
+}
+
+func divide(a int, b int) int {
+	return a / b
+}
+
+func main() {
+	var firstOperand, secondOperand int
+	var operator string
+
+	_, err := fmt.Scanln(&firstOperand)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scanln(&secondOperand)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err = fmt.Scanln(&operator)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	var result int
+
+	switch operator {
+	case "+":
+		result = add(firstOperand, secondOperand)
+
+	case "-":
+		result = subtract(firstOperand, secondOperand)
+
+	case "*":
+		result = multiply(firstOperand, secondOperand)
+
+	case "/":
+		if secondOperand == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+
+		result = divide(firstOperand, secondOperand)
+
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	fmt.Println(result)
+}
---
+++ alex.somov/task-1/go.mod
@@ -0,0 +1,3 @@
+module newProject_calc
+
+go 1.22.7
