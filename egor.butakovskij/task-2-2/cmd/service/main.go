96b235aa5495aa465b97d040d909b9b0b14ddd57
egor butakovskij <mrtntkatz@gmail.com>
2025-10-13 16:59:07 +0300

    [TASK-2-2] fix some errors

--- egor.butakovskij/task-2-2/cmd/service/main.go
+++ egor.butakovskij/task-2-2/cmd/service/main.go
@@ -3,54 +3,56 @@ package main
 import (
 	"container/heap"
 	"fmt"
+	"log"
 )
 
 type IntMaxHeap []int
 
-func (myHeap *IntMaxHeap) Len() int {
-	return len(*myHeap)
+func (h *IntMaxHeap) Len() int {
+	return len(*h)
 }
 
-func (myHeap *IntMaxHeap) Less(i, j int) bool {
-	return (*myHeap)[i] > (*myHeap)[j]
+func (h *IntMaxHeap) Less(i, j int) bool {
+	return (*h)[i] > (*h)[j]
 }
 
-func (myHeap *IntMaxHeap) Swap(i, j int) {
-	(*myHeap)[i], (*myHeap)[j] = (*myHeap)[j], (*myHeap)[i]
+func (h *IntMaxHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
-func (myHeap *IntMaxHeap) Push(x interface{}) {
-	value, ok := x.(int)
-	if !ok {
-		return
+func (h *IntMaxHeap) Push(x interface{}) {
+	if value, ok := x.(int); ok {
+		*h = append(*h, value)
 	}
-
-	*myHeap = append(*myHeap, value)
 }
 
-func (myHeap *IntMaxHeap) Pop() interface{} {
-	old := *myHeap
-	n := len(old)
-	x := old[n-1]
-	*myHeap = old[0 : n-1]
+func (h *IntMaxHeap) Pop() interface{} {
+	if h.Len() == 0 {
+		return nil
+	}
+	n := h.Len() - 1
+	x := (*h)[n]
+	*h = (*h)[:n]
 
 	return x
 }
 
 func main() {
+	log.SetFlags(0)
+
 	var count, value, numberOfDish int
 
 	myHeap := &IntMaxHeap{}
 
 	_, err := fmt.Scan(&count)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalf("error: %v", err)
 	}
 
 	for range count {
 		_, err = fmt.Scan(&value)
 		if err != nil {
-			fmt.Println(err)
+			log.Fatalf("error: %v", err)
 		}
 
 		heap.Push(myHeap, value)
@@ -60,11 +62,19 @@ func main() {
 
 	_, err = fmt.Scan(&numberOfDish)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalf("error: %v", err)
 	}
 
 	for i := 1; i < numberOfDish; i++ {
+		if myHeap.Len() == 0 {
+			log.Fatal("heap is empty")
+		}
+
 		heap.Pop(myHeap)
+	}
+
+	if myHeap.Len() == 0 {
+		log.Fatal("heap is empty")
 	}
 
 	fmt.Println(heap.Pop(myHeap))
