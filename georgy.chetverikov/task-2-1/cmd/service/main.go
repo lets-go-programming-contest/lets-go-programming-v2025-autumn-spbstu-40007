0898d3aa733351032f09e2621abb4f0a85e95417
Georgy Chetverikov <blessedcakee@outlook.com>
2025-10-14 16:08:02 +0300

    [TASK-2-1] linter linter linter

--- georgy.chetverikov/task-2-1/cmd/service/main.go
+++ georgy.chetverikov/task-2-1/cmd/service/main.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-func main() {
+func main() {//nolint:funlen
 	const (
 		lowerBoundary  = 15
 		upperBoundary  = 30
@@ -33,14 +33,14 @@ func main() {
 			if hasError {
 				fmt.Println(-1)
 				reader.ReadString('\n') //nolint:errcheck
-				// сделано для того, чтобы просто пропустить ввод в случае чего, но линтер ругается на то, что не ловлю ошибку, поэтому считаю, что тут можно это сделать. //nolinter:lll
+				// сделано для того, чтобы просто пропустить ввод в случае чего, но линтер ругается на то, что не ловлю ошибку, поэтому считаю, что тут можно это сделать. //nolint:lll
 
 				continue
 			}
 			settingsTemp, err := reader.ReadString('\n')
-			if err != nil {
+			if err != nil {//nolint:wsl
 				fmt.Println(-1)
-				hasError = true //nolinter:wsl
+				hasError = true //nolint:wsl
 
 				continue
 			}
@@ -49,7 +49,7 @@ func main() {
 			if len(parts) < settingsLength { //nolint:wsl
 				fmt.Println(-1)
 				hasError = true //nolint:wsl
-				// линтер ругается на то, что присваивание не стоит вместе с другими присваиваниями, оно тут не нужно и я, честно говоря, и не знаю как ему угодить, так что пишу нолинт. //nolinter:lll
+				// линтер ругается на то, что присваивание не стоит вместе с другими присваиваниями, оно тут не нужно и я, честно говоря, и не знаю как ему угодить, так что пишу нолинт. //nolint:lll
 
 				continue
 			}
@@ -62,7 +62,7 @@ func main() {
 
 				return parts[0], temperature
 			}()
-			// сразу поясню, для чего тут это. Линтер ругался на то, что у меня до if statment'a идут два присваивания, и поэтому, долго не ломая голову, решил сделать так, как у меня было изначально в каком-то из первых коммитов.
+			// сразу поясню, для чего тут это. Линтер ругался на то, что у меня до if statment'a идут два присваивания, и поэтому, долго не ломая голову, решил сделать так, как у меня было изначально в каком-то из первых коммитов. //nolint:lll
 
 			if sign == ">=" {
 				lowerBorder = max(lowerBorder, temperature)
@@ -72,7 +72,7 @@ func main() {
 
 			if lowerBorder > upperBorder {
 				fmt.Println(-1)
-				hasError = true
+				hasError = true //nolint:wsl
 			} else {
 				fmt.Println(lowerBorder)
 			}
