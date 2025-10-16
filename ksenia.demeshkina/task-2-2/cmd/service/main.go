9d837633f32cfbb42dd1b41f6f30bec781a04007
Ксения Демешкина <demeshkinakseniaa@gmail.com>
2025-10-16 20:30:25 +0300

    [TASK-2-2] fix lint

--- ksenia.demeshkina/task-2-2/cmd/service/main.go
+++ ksenia.demeshkina/task-2-2/cmd/service/main.go
@@ -8,9 +8,9 @@ import (
 
 type IntHeap []int
 
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return (h)[i] < (h)[j] }
-func (h IntHeap) Swap(i, j int)      { (h)[i], (h)[j] = (h)[j], (h)[i] }
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
 func (h *IntHeap) Push(x any) {
 	if value, ok := x.(int); ok {
