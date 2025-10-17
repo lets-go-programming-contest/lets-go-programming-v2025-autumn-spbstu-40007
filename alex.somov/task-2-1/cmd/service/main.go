cc5dc6823d3327c51c5888b936aacdffaa82e086
Alex Somov <somov.2015@mail.ru>
2025-10-17 12:59:47 +0300

    [TASK-2-1] Initial commit

---
+++ alex.somov/task-2-1/cmd/service/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+)
+
+func findOptimalTemp(numOfEmployees int) {
+	minTemperature := 15
+	maxTemperature := 30
+
+	for range numOfEmployees {
+		var sign string
+		var temperature int
+
+		_, err := fmt.Scanln(&sign, &temperature)
+		if err != nil || (sign != ">=" && sign != "<=") {
+			fmt.Println(-1)
+			continue
+		}
+
+		if sign == ">=" {
+			minTemperature = max(temperature, minTemperature)
+		}
+
+		if sign == "<=" {
+			maxTemperature = min(temperature, maxTemperature)
+		}
+
+		if minTemperature > maxTemperature {
+			fmt.Println(-1)
+		} else {
+			fmt.Println(minTemperature)
+		}
+	}
+}
+
+func main() {
+	var numOfDepartments, numOfEmployees int
+
+	_, err := fmt.Scanln(&numOfDepartments)
+	if err != nil || numOfDepartments > 1000 || numOfDepartments < 1 {
+		fmt.Println(-1)
+		return
+	}
+
+	for range numOfDepartments {
+		_, err = fmt.Scanln(&numOfEmployees)
+		if err != nil || numOfEmployees > 1000 || numOfEmployees < 1 {
+			fmt.Println(-1)
+			continue
+		}
+
+		findOptimalTemp(numOfEmployees)
+	}
+}
