3cdd2257b4573548611821cac4b67ec78937a7c3
Казаков Ярослав <yarikkotorikot@gmail.com>
2025-10-18 00:04:38 +0300

    [TASK-2-1] ran gofmt on the code

--- yaroslav.kazakov/task-2-1/cmd/service/main.go
+++ yaroslav.kazakov/task-2-1/cmd/service/main.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 )
 
-func main(){
-	var N,K int
+func main() {
+	var N, K int
 	_, err := fmt.Scanln(&N)
 	if err != nil {
 		fmt.Println("Invalid N")
 		return
 	}
-	for i := 0; i < N; i++{
+	for i := 0; i < N; i++ {
 		_, err = fmt.Scanln(&K)
 		if err != nil {
 			fmt.Println("Invalid K")
@@ -19,7 +19,7 @@ func main(){
 		}
 		var max = 30
 		var min = 15
-		for i := 0; i < K; i++{
+		for i := 0; i < K; i++ {
 			var op string
 			var T int
 			_, err := fmt.Scan(&op, &T)
@@ -27,27 +27,26 @@ func main(){
 				fmt.Println("invalid Temp input")
 				return
 			}
-			
-			switch(op){
-				case ">=":
-					if T > min{
-						min = T
-					}
-				case "<=":
-					if T < max{
-						max = T
-					}
-				default:
-					fmt.Println("Invalid: >=/<= expected")
-					return
+
+			switch op {
+			case ">=":
+				if T > min {
+					min = T
+				}
+			case "<=":
+				if T < max {
+					max = T
+				}
+			default:
+				fmt.Println("Invalid: >=/<= expected")
+				return
 			}
 			if min > max {
 				fmt.Println(-1)
-			}else{
+			} else {
 				fmt.Println(min)
 			}
 		}
 	}
-	
-	
+
 }
