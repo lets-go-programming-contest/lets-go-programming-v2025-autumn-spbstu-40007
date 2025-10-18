1adb2ccca7f0ff4bc2ea5a5096650909d88aa50c
Amirkhan Myasoutov <>
2025-10-19 00:47:31 +0300

    [TASK-2-2] Lint fix 4

--- amirkhan.myasoutov/task-2-2/cmd/service/main.go
+++ amirkhan.myasoutov/task-2-2/cmd/service/main.go
@@ -14,9 +14,10 @@ func (h DishRating) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 func (h *DishRating) Push(x any) {
 	value, ok := x.(int)
 	if !ok {
+
 		return
 	}
-	
+
 	*h = append(*h, value)
 }
 
@@ -32,39 +33,48 @@ func (h *DishRating) Pop() any {
 func main() {
 	var dishAmount int
 	_, err := fmt.Scanln(&dishAmount)
+
 	if err != nil {
 		fmt.Println("Invalid number of dishes")
+
 		return
 	}
 
 	if dishAmount < 1 || dishAmount > 10000 {
 		fmt.Println("Invalid number of dishes")
+
 		return
 	}
 
 	dishQueue := make([]int, dishAmount)
 	for index := range dishQueue {
 		_, err = fmt.Scan(&dishQueue[index])
+
 		if err != nil {
 			fmt.Println("Invalid dish rating")
+
 			return
 		}
 
 		if dishQueue[index] < -10000 || dishQueue[index] > 10000 {
 			fmt.Println("Invalid dish rating")
+
 			return
 		}
 	}
 
 	var preferenceNumber int
 	_, err = fmt.Scan(&preferenceNumber)
+	
 	if err != nil {
 		fmt.Println("Invalid number of preference")
+
 		return
 	}
 
 	if preferenceNumber < 0 || preferenceNumber > dishAmount {
 		fmt.Println("Invalid number of preference")
+
 		return
 	}
 
@@ -74,6 +84,7 @@ func main() {
 	for _, value := range dishQueue {
 		if dishRating.Len() < preferenceNumber {
 			heap.Push(dishRating, value)
+
 			continue
 		}
 
