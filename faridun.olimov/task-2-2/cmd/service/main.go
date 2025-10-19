dacec319a27fd3615fef6b1e1d65dd351e354561
RediskaKrasnaya <77031135+Maska192@users.noreply.github.com>
2025-10-19 17:15:27 +0300

    [TASK-2-2] (#37)
    * [TASK-2-2] Initial commit
    
    * [TASK-2-2] lint fixing
    
    * [TASK-2-2] fixing  errors

---
+++ faridun.olimov/task-2-2/cmd/service/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+	"os"
+)
+
+type IntHeap []int
+
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
+func (h *IntHeap) Push(x any) {
+	if num, ok := x.(int); ok {
+		*h = append(*h, num)
+	}
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+
+	return x
+}
+
+func main() {
+	var count, kValue int
+
+	_, err := fmt.Scan(&count)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+
+		return
+	}
+
+	numbers := make([]int, count)
+
+	for i := range count {
+		_, err = fmt.Scan(&numbers[i])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+
+			return
+		}
+	}
+
+	_, err = fmt.Scan(&kValue)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+
+		return
+	}
+
+	intHeap := &IntHeap{}
+	heap.Init(intHeap)
+
+	for _, num := range numbers {
+		heap.Push(intHeap, num)
+	}
+
+	for i := 1; i < kValue; i++ {
+		heap.Pop(intHeap)
+	}
+
+	fmt.Println((*intHeap)[0])
+}
