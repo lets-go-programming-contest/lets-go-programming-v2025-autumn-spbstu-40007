112ee5246d8c14e9cd7947f14a4d46f33c6ec876
itsdasha <m.astafeva07081985@gmail.com>
2025-10-19 18:29:03 +0300

    [TASK-2-2] (#43)
    * [TASK-2-2] Initial commit
    
    * [TASK-2-2] lint fixing
    
    * [TASK-2-2] lint fixing
    
    * [TASK-2-2] lint fixing
    
    * [TASK-2-2] lint fixing
    
    * [TASK-2-2] lint fixing
    
    * [TASK-2-2] fixing
    
    * [TASK-2-2] lint fixing

---
+++ daria.astafyeva/task-2-2/cmd/service/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type DishHeap []int
+
+func (h *DishHeap) Len() int           { return len(*h) }
+func (h *DishHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
+func (h *DishHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
+func (h *DishHeap) Push(x any) {
+	if val, ok := x.(int); ok {
+		*h = append(*h, val)
+	}
+}
+
+func (h *DishHeap) Pop() any {
+	heapArr := *h
+	length := len(heapArr)
+
+	if length == 0 {
+		return nil
+	}
+
+	last := heapArr[length-1]
+	*h = heapArr[:length-1]
+
+	return last
+}
+
+func main() {
+	var numDishes, preferenceRank int
+	if _, err := fmt.Scan(&numDishes); err != nil {
+		fmt.Println("Invalid input:", err)
+
+		return
+	}
+
+	dishPreferenceHeap := make(DishHeap, numDishes)
+	for i := range numDishes {
+		if _, err := fmt.Scan(&dishPreferenceHeap[i]); err != nil {
+			fmt.Println("Invalid input:", err)
+
+			return
+		}
+	}
+
+	if _, err := fmt.Scan(&preferenceRank); err != nil {
+		fmt.Println("Invalid input:", err)
+
+		return
+	}
+
+	heap.Init(&dishPreferenceHeap)
+
+	for range preferenceRank - 1 {
+		heap.Pop(&dishPreferenceHeap)
+	}
+
+	if dishPreferenceHeap.Len() > 0 {
+		fmt.Println(dishPreferenceHeap[0])
+	} else {
+		fmt.Println("Invalid input")
+	}
+}
---
+++ daria.astafyeva/task-2-2/go.mod
@@ -0,0 +1,3 @@
+module github.com/itsdasha/task-2-2
+
+go 1.22.7
