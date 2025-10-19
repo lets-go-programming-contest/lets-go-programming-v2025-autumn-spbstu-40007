81d292785f660fb9e2f1a3f78ccf5de289f1df05
UwUshkin <ekaterinanazimova07@gmail.com>
2025-10-19 19:24:38 +0300

    [TASK-2-2] (#65)
    * [TASK-2-2] Initial commit
    
    * [TASK-2-2] Add go mod
    
    * [TASK-2-2] Fix lint
    
    * [TASK-2-2] Fix lint 2
    
    * [TASK-2-2] Fix code
    
    * [TASK-2-2] Fix code 2
    
    * [TASK-2-2] Fix lint 3
    
    * [TASK-2-2] Fix lint 4
    
    * [TASK-2-2] Fix lint 5
    
    * [TASK-2-2] Fix lint 6
    
    * [TASK-2-2] Fix lint 7
    
    * [TASK-2-2] Fix lint 8
    
    * [TASK-2-2] Fix lint 9
    
    * [TASK-2-2] Fix lint 10
    
    * [TASK-2-2] Fix lint 11
    
    * [TASK-2-2] Fix lint 12
    
    * [TASK-2-2] Fix lint 13
    
    * [TASK-2-2] Fix lint 14
    
    * [TASK-2-2] Fix lint 15
    
    * [TASK-2-2] Fix lint 16
    
    * [TASK-2-2] Fix 1
    
    * [TASK-2-2] Fix 1
    
    * [TASK-2-2] Fix 1
    
    * [TASK-2-2] Fix 1
    
    * [TASK-2-2] Fix 1
    
    * [TASK-2-2] Fix 2
    
    * [TASK-2-2] Fix 3
    
    * [TASK-2-2] Fix 3
    
    * [TASK-2-2] Fix 3
    
    * [TASK-2-2] Fix 3
    
    * [TASK-2-2] Fix 3
    
    * [TASK-2-2] Fix 3

---
+++ ekaterina.nazimova/task-2-2/cmd/service/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type IntHeap []int
+
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	if value, ok := x.(int); ok {
+		*h = append(*h, value)
+	}
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+
+	return x
+}
+
+func main() {
+	var dishAmount int
+
+	_, err := fmt.Scan(&dishAmount)
+	if err != nil {
+		fmt.Println(err)
+
+		return
+	}
+
+	dishHeap := &IntHeap{}
+	*dishHeap = make([]int, 0, dishAmount)
+	heap.Init(dishHeap)
+
+	for range make([]int, dishAmount) {
+		var dish int
+
+		_, err = fmt.Scan(&dish)
+		if err != nil {
+			fmt.Println(err)
+
+			return
+		}
+
+		heap.Push(dishHeap, dish)
+	}
+
+	var dishNumber int
+
+	_, err = fmt.Scan(&dishNumber)
+	if err != nil {
+		fmt.Println(err)
+
+		return
+	}
+
+	var kthDish interface{}
+
+	for range make([]int, dishNumber) {
+		kthDish = heap.Pop(dishHeap)
+	}
+
+	fmt.Println(kthDish)
+}
