deda43da0c018bec567d87a5a464a72c0b7c0a35
UwUshkin <ekaterinanazimova07@gmail.com>
2025-10-19 18:49:22 +0300

    [TASK-2-1] (#52)
    * [TASK-2-1] Initial commit
    
    * [TASK-2-1] Fix linter
    
    * [TASK-2-1] Fix folder
    
    * [TASK-2-1] :(
    
    * [TASK-2-1] Fix linter 2
    
    * [TASK-2-1] Fix linter 3
    
    * [TASK-2-1] Fix linter 4 :(
    
    * [TASK-2-1] Fix linter 5..?
    
    * [TASK-2-1] Fix linter 6...
    
    * [TASK-2-1] Fix linter 7
    
    * [TASK-2-1] Fix linter 8
    
    * [TASK-2-1] Fix linter 9
    
    * [TASK-2-1] Fix linter 10
    
    * [TASK-2-1] Fix
    
    * [TASK-2-1] Fix 2
    
    * [TASK-2-1] Fix 3
    
    * [TASK-2-1] Fix 4
    
    * [TASK-2-1] Fix lint
    
    * [TASK-2-1] Fix lint
    
    * [TASK-2-1] Fix lint
    
    * [TASK-2-1] Fix lint
    
    * [TASK-2-1] Fix lint
    
    * [TASK-2-1] Fix lint
    
    * [TASK-2-1] Fix lint
    
    * [TASK-2-1] Fix lint
    
    * [TASK-2-1] Fix lint
    
    * [TASK-2-1] Fix lint

---
+++ ekaterina.nazimova/task-2-1/cmd/service/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var departmentsCount int
+
+	if _, err := fmt.Scan(&departmentsCount); err != nil {
+		fmt.Println("Input error: failed to read department count -", err)
+
+		return
+	}
+
+	for range departmentsCount {
+		var employeesCount int
+
+		if _, err := fmt.Scan(&employeesCount); err != nil {
+			fmt.Println("Input error: failed to read employees count -", err)
+
+			return
+		}
+
+		minTemp := 15
+		maxTemp := 30
+
+		for range employeesCount {
+			var (
+				operator  string
+				tempValue int
+			)
+
+			if _, err := fmt.Scan(&operator, &tempValue); err != nil {
+				fmt.Println("Input error: failed to read operator and temperature value -", err)
+
+				return
+			}
+
+			if operator == ">=" && tempValue > minTemp {
+				minTemp = tempValue
+			}
+
+			if operator == "<=" && tempValue < maxTemp {
+				maxTemp = tempValue
+			}
+
+			if minTemp > maxTemp {
+				fmt.Println(-1)
+
+				continue
+			}
+
+			fmt.Println(minTemp)
+		}
+	}
+}
