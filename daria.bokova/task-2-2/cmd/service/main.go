e7a94bf7803dfcb5e1df16e7e9be729ab2b7fbf2
bdshka <bokovad2@gmail.com>
2025-10-19 17:52:19 +0300

    [TASK-2-2]  (#74)
    * [TASK-2-2] Initial commit
    
    * [TASK-2-2] lint fixing
    
    * [TASK-2-2] Initial commit
    
    * [TASK-2-2] Initial commit
    
    * [TASK-2-2] Initial commit

---
+++ daria.bokova/task-2-2/cmd/service/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type MaxHeap struct {
+	elements []int
+}
+
+func (h *MaxHeap) Len() int { return len(h.elements) }
+
+func (h *MaxHeap) Less(i, j int) bool {
+	return h.elements[i] > h.elements[j]
+}
+
+func (h *MaxHeap) Swap(i, j int) {
+	h.elements[i], h.elements[j] = h.elements[j], h.elements[i]
+}
+
+func (h *MaxHeap) Push(x interface{}) {
+	if val, ok := x.(int); ok {
+		h.elements = append(h.elements, val)
+	}
+}
+
+func (h *MaxHeap) Pop() interface{} {
+	old := h.elements
+	n := len(old)
+	item := old[n-1]
+	h.elements = old[0 : n-1]
+
+	return item
+}
+
+func (h *MaxHeap) Peek() int {
+	return h.elements[0]
+}
+
+func main() {
+	var totalNumbers, targetPosition int
+
+	if _, err := fmt.Scan(&totalNumbers); err != nil {
+		return
+	}
+
+	numberSequence := make([]int, totalNumbers)
+	for idx := range totalNumbers {
+		if _, err := fmt.Scan(&numberSequence[idx]); err != nil {
+			fmt.Println("Error reading number at position")
+
+			return
+		}
+	}
+
+	if _, err := fmt.Scan(&targetPosition); err != nil {
+		fmt.Printf("Error reading target position")
+
+		return
+	}
+
+	numHeap := &MaxHeap{elements: []int{}}
+	heap.Init(numHeap)
+
+	for _, value := range numberSequence {
+		heap.Push(numHeap, value)
+	}
+
+	for extractCount := 1; extractCount < targetPosition; extractCount++ {
+		heap.Pop(numHeap)
+	}
+
+	fmt.Println(numHeap.Peek())
+}
