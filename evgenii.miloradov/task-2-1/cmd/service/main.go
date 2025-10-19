2817848be65642f90db8ca34b13d04c0000c3411
EvgeniiMilo <76951434+EvgeniiMilo@users.noreply.github.com>
2025-10-19 22:04:44 +0300

    [TASK-2-1]  (#96)
    * [TASK-2-1] Initial Commit
    
    * [TASK-2-1] Var's scan changed
    
    * [TASK-2-1] Var's scan changed
    
    * [TASK-2-1] Sorry for lint
    
    * [TASK-2-1] nolint for range
    
    * [TASK-2-1] nolint for range and space
    
    * [TASK-2-1] nolint for range, add space
    
    * [TASK-2-1] add space
    
    * [TASK-2-1] del space

---
+++ evgenii.miloradov/task-2-1/cmd/service/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var numOfState, numOfEmp int
+
+	var maxTemp, minTemp int
+
+	if _, err := fmt.Scan(&numOfState); err != nil {
+		fmt.Println(-1)
+
+		return
+	}
+
+	//nolint:intrange
+	for i := 0; i < numOfState; i++ {
+		maxTemp = 15
+		minTemp = 30
+
+		if _, err := fmt.Scan(&numOfEmp); err != nil {
+			fmt.Println(-1)
+
+			return
+		}
+
+		//nolint:intrange
+		for j := 0; j < numOfEmp; j++ {
+			var operand string
+
+			var comfTemp int
+
+			_, err := fmt.Scan(&operand, &comfTemp)
+			if err != nil {
+				fmt.Println("-1")
+
+				return
+			}
+
+			switch operand {
+			case ">=":
+				if comfTemp > maxTemp {
+					maxTemp = comfTemp
+				}
+			case "<=":
+				if comfTemp < minTemp {
+					minTemp = comfTemp
+				}
+			default:
+				fmt.Println("-1")
+
+				return
+			}
+
+			if maxTemp <= minTemp {
+				fmt.Println(maxTemp)
+			} else {
+				fmt.Println(-1)
+			}
+		}
+	}
+}
