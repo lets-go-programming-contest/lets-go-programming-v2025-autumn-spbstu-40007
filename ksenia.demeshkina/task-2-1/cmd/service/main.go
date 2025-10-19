dd7d1c9a9e5c3c9e5f71a4a0c52d8340b8f2dc06
Ксеня <demeshkinakseniaa@gmail.com>
2025-10-19 18:36:38 +0300

    [TASK-2-1] (#44)
    * [TASK-2-1] Initial commit
    
    * [TASK-2-1]fix errors
    
    * [TASK-2-1] fix lint
    
    * [TASK-2-1]fix errors
    
    * [TASK-2-1]linters again
    
    * [TASK-2-1]linter error attempt 100
    
    * [TASK-2-1] final attempt
    
    * [TASK-2-1] the final final attempt
    
    * [TASK-2-1] again
    
    * [TASK-2-1] again 2
    
    * [TASK-2-1] fix comments
    
    * [TASK-2-1] fix lint

---
+++ ksenia.demeshkina/task-2-1/cmd/service/main.go
@@ -0,0 +1,87 @@
+package main
+
+import "fmt"
+
+func temperature(workers int) error {
+	minTemp := 15
+	maxTemp := 30
+
+	for iterator := range workers {
+		var operator string
+
+		var value int
+
+		_, err := fmt.Scan(&operator, &value)
+		if err != nil {
+			return fmt.Errorf("error reading operator and value %w", err)
+		}
+
+		if operator == "<=" {
+			if value < maxTemp {
+				maxTemp = value
+			}
+		}
+
+		if operator == ">=" {
+			if value > minTemp {
+				minTemp = value
+			}
+		}
+
+		if minTemp > maxTemp {
+			fmt.Println(-1)
+
+			for j := iterator + 1; j < workers; j++ {
+				_, err := fmt.Scan(&operator, &value)
+				if err != nil {
+					return fmt.Errorf("error reading remaining data %w", err)
+				}
+
+				fmt.Println(-1)
+			}
+
+			return nil
+		}
+
+		fmt.Println(minTemp)
+	}
+
+	return nil
+}
+
+func main() {
+	var department, workers int
+
+	_, err := fmt.Scan(&department)
+	if err != nil {
+		fmt.Println("Invalid department input format")
+	}
+
+	if department < 1 || department > 1000 {
+		fmt.Println("The number of departments must be in the range from 1 to 1000")
+
+		return
+	}
+
+	for range department {
+		_, err = fmt.Scan(&workers)
+		if err != nil {
+			fmt.Println("Invalid employee input format")
+
+			return
+		}
+
+		if workers < 1 || workers > 1000 {
+			fmt.Println("The number of employees must be from 1 to 1000")
+
+			return
+		}
+
+		err := temperature(workers)
+		if err != nil {
+			fmt.Println("error: ", err)
+
+			return
+		}
+	}
+}
