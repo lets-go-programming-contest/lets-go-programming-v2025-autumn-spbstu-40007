3d7ccc22af9f3875c0ce914ec8e82fefd5785308
EvgeniiMilo <76951434+EvgeniiMilo@users.noreply.github.com>
2025-10-19 22:08:44 +0300

    [TASK-2-2]  (#97)
    * [TASK-2-2] Initial Commit
    
    * [TASK-2-2] rename file
    
    * [TASK-2-2] lint_troubles
    
    * [TASK-2-2] lint_troubles

---
+++ evgenii.miloradov/task-2-2/cmd/service/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+//nolint:recvcheck
+type stdHeap []int
+
+func (ourHeap stdHeap) Len() int {
+	return len(ourHeap)
+}
+
+func (ourHeap stdHeap) Less(i, j int) bool {
+	return ourHeap[i] < ourHeap[j]
+}
+
+func (ourHeap stdHeap) Swap(i, j int) {
+	ourHeap[i], ourHeap[j] = ourHeap[j], ourHeap[i]
+}
+
+func (ourHeap *stdHeap) Push(x any) {
+	//nolint:forcetypeassert
+	*ourHeap = append(*ourHeap, x.(int))
+}
+
+func (ourHeap *stdHeap) Pop() any {
+	old := *ourHeap
+	n := len(old)
+	x := old[n-1]
+	*ourHeap = old[:n-1]
+
+	return x
+}
+
+func main() {
+	var numOfNumbers, indexK int
+
+	if _, err := fmt.Scan(&numOfNumbers); err != nil {
+		fmt.Println(-1)
+
+		return
+	}
+
+	numberOfMeals := make([]int, numOfNumbers)
+
+	//nolint:intrange
+	for i := 0; i < numOfNumbers; i++ {
+		if _, err := fmt.Scan(&numberOfMeals[i]); err != nil {
+			fmt.Println(-1)
+
+			return
+		}
+	}
+
+	if _, err := fmt.Scan(&indexK); err != nil {
+		fmt.Println(-1)
+
+		return
+	}
+
+	ourHeap := &stdHeap{}
+
+	heap.Init(ourHeap)
+
+	//nolint:intrange
+	for i := 0; i < indexK; i++ {
+		heap.Push(ourHeap, numberOfMeals[i])
+	}
+
+	for i := indexK; i < numOfNumbers; i++ {
+		if numberOfMeals[i] > (*ourHeap)[0] {
+			heap.Pop(ourHeap)
+			heap.Push(ourHeap, numberOfMeals[i])
+		}
+	}
+
+	fmt.Println((*ourHeap)[0])
+}
