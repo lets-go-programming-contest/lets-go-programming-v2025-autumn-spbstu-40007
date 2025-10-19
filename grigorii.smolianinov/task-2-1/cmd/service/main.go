2af5a943d5035de12dece5774b356e11fd8c0bd6
Smolyaninoff <smolyaninovgrg@gmail.com>
2025-10-19 19:37:32 +0300

    [TASK-2-1] (#67)
    * [TASK-2-1] initial commit
    
    * [TASK-2-1]
    
    * [TASK-2-1]
    
    * [TASK-2-1] Bug fixes
    
    * [TASK-2-1] Bug fixes
    
    * [TASK-2-1] Bug fixes
    
    * [TASK-2-1] Bug fixes
    
    * [TASK-2-1] Bug fixes
    
    * [TASK-2-1] Bug fixes
    
    * [TASK-2-1] Bug fixes
    
    * [TASK-2-1] Bug fixes
    
    * [TASK-2-1] Bug fixes
    
    * [TASK-2-1] Bug fixes
    
    * [TASK-2-1] Bug fixes
    
    * [TASK-2-1] Bugs fixes
    
    * [TASK-2-1] ...
    
    * [TASK-2-1] ...
    
    * [TASK-2-1] ...
    
    * [TASK-2-1] ...
    
    * [TASK-2-1] ...
    
    * [TASK-2-1] ...
    
    * [TASK-2-1] ...

---
+++ grigorii.smolianinov/task-2-1/cmd/service/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var departmentsCount, employeesCount int
+
+	if _, err := fmt.Fscan(os.Stdin, &departmentsCount); err != nil {
+		return
+	}
+
+	for range departmentsCount {
+		if _, err := fmt.Fscan(os.Stdin, &employeesCount); err != nil {
+			return
+		}
+
+		minTemp, maxTemp := 15, 30
+		rangeValid := true
+
+		for range employeesCount {
+			var (
+				sign string
+				temp int
+			)
+
+			if _, err := fmt.Fscan(os.Stdin, &sign, &temp); err != nil {
+				return
+			}
+
+			if !rangeValid {
+				fmt.Println(-1)
+
+				continue
+			}
+
+			if sign == ">=" && temp > minTemp {
+				minTemp = temp
+			}
+
+			if sign == "<=" && temp < maxTemp {
+				maxTemp = temp
+			}
+
+			if minTemp <= maxTemp {
+				fmt.Println(minTemp)
+			} else {
+				fmt.Println(-1)
+
+				rangeValid = false
+			}
+		}
+	}
+}
