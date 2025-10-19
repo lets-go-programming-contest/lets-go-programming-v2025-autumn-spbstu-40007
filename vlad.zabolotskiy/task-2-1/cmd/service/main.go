22a57a5cf8cc5eca36a5772ab734e00e14a94013
Vladislav <se1lzor.06@gmail.com>
2025-10-19 22:19:39 +0300

    [TASK-2-1] Initial commit (#102)
    Co-authored-by: Vladislav Zabolotskiy <se1lzor@MacBook-Air-Vladislav.local>

---
+++ vlad.zabolotskiy/task-2-1/cmd/service/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	minTempConst        = 15
+	maxTempConst        = 30
+	opMoreConst         = ">="
+	opLessConst         = "<="
+	expectedFieldsCount = 2
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	quantityDepartments, _ := reader.ReadString('\n')
+	departments, _ := strconv.Atoi(strings.TrimSpace(quantityDepartments))
+
+	if departments < 1 || departments > 1000 {
+		fmt.Println("Invalid departments range")
+
+		return
+	}
+
+	optimalTemperature(reader, departments)
+}
+
+func optimalTemperature(reader *bufio.Reader, departments int) {
+	for range departments {
+		quantityEmployees, _ := reader.ReadString('\n')
+		employees, _ := strconv.Atoi(strings.TrimSpace(quantityEmployees))
+
+		if employees < 1 || employees > 1000 {
+			fmt.Println("Invalid employees range")
+
+			return
+		}
+
+		minTemp := minTempConst
+		maxTemp := maxTempConst
+
+		for range employees {
+			preference, _ := reader.ReadString('\n')
+			preference = strings.TrimSpace(preference)
+
+			data := strings.Fields(preference)
+
+			if len(data) != expectedFieldsCount {
+				minTemp = maxTemp + 1
+
+				continue
+			}
+
+			operator := data[0]
+
+			temperature, err := strconv.Atoi(data[1])
+			if err != nil {
+				minTemp = maxTemp + 1
+
+				continue
+			}
+
+			switch operator {
+			case opMoreConst:
+				minTemp = max(minTemp, temperature)
+			case opLessConst:
+				maxTemp = min(maxTemp, temperature)
+			default:
+				minTemp = maxTemp + 1
+
+				continue
+			}
+
+			if minTemp > maxTemp {
+				fmt.Println(-1)
+			} else {
+				fmt.Println(minTemp)
+			}
+		}
+	}
+}
