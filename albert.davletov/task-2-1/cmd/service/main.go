6fa9b67aa9767af1dc958bf00e1d0993062497d9
treadwave <iruthleesredx@gmail.com>
2025-10-19 16:30:10 +0300

    [TASK-2-1]  (#34)
    * [TASK-2-1] Initial commit.
    
    * [TASK-2-1] Fixed errors
    
    * [TASK-2-1] Changed linter errors
    
    * [TASK-2-1] Fixed linter errors
    
    * [TASK-2-1] Changed a bit
    
    * [TASK-2-1] fixed
    
    * [TASK-2-1] fixed
    
    * [TASK-2-1] fixed
    
    * [TASK-2-1] fixed
    
    * [TASK-2-1] fixed

---
+++ albert.davletov/task-2-1/cmd/service/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var numberDepartments int
+
+	_, err := fmt.Scan(&numberDepartments)
+	if err != nil {
+		fmt.Println("Error reading input: ", err)
+
+		return
+	}
+
+	for range numberDepartments {
+		var (
+			numberEmployees, temperature int
+			operand                      string
+		)
+
+		_, err := fmt.Scan(&numberEmployees)
+		if err != nil {
+			fmt.Println("Error reading input: ", err)
+
+			return
+		}
+
+		minTemperature, maxTemperature := 15, 30
+
+		for range numberEmployees {
+			_, err := fmt.Scan(&operand, &temperature)
+			if err != nil {
+				fmt.Println("Error reading input: ", err)
+
+				return
+			}
+
+			if operand == "<=" {
+				maxTemperature = min(maxTemperature, temperature)
+			} else {
+				minTemperature = max(temperature, minTemperature)
+			}
+
+			if minTemperature > maxTemperature {
+				fmt.Println(-1)
+			} else {
+				fmt.Println(minTemperature)
+			}
+		}
+	}
+}
