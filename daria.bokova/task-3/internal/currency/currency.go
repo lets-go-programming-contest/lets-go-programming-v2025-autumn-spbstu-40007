8b96f855c8e4e282fff2c2ea0ad035537cd648c5
Bokova Daria <bokovad2@gmail.com>
2025-10-20 15:36:14 +0300

    [TASK-3] Initial commit

---
+++ daria.bokova/task-3/internal/currency/currency.go
@@ -0,0 +1,14 @@
+package currency
+
+import "encoding/xml"
+
+type ValCurs struct {
+	XMLName xml.Name `xml:"ValCurs"`
+	Valutes []Valute `xml:"Valute"`
+}
+
+type Valute struct {
+	NumCode  string `xml:"NumCode"`
+	CharCode string `xml:"CharCode"`
+	Value    string `xml:"Value"`
+}
