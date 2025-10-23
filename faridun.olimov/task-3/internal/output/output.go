11f71925e958193c9ca02a63f8e96d4475056a18
Олимов Фаридун <maska192yt@mail.ru>
2025-10-24 00:13:44 +0300

    [TASK-3] fix build

--- faridun.olimov/task-3/internal/output/output.go
+++ faridun.olimov/task-3/internal/output/output.go
@@ -22,7 +22,7 @@ func saveResults(filePath string, format string, data []data.ResultValute) {
 	case "yaml":
 		encodedData, err = yaml.Marshal(data)
 	case "xml":
-		resultXML := data.ResultValutes{Valutes: data}
+		resultXML := ResultValutes{Valutes: data}
 		encodedData, err = xml.MarshalIndent(resultXML, "", "  ")
 		encodedData = []byte(xml.Header + string(encodedData))
 	default:
