55c1cefc5b63f621dba82c1eda593ff13f4de648
Konev Dmitri <konevdmitrij791@gmail.com>
2025-10-25 01:27:19 +0300

    [TASK-2-1] Update solution with linter fixes

--- dmitry.konev/task-2-1/cmd/service/main.go
+++ dmitry.konev/task-2-1/cmd/service/main.go
@@ -9,88 +9,151 @@ import (
 )
 
 const (
-	minTemp       = 15
-	maxTemp       = 30
+	minTemp = 15
+
+	maxTemp = 30
+
 	expectedParts = 2
 )
 
 func processDepartment(scanner *bufio.Scanner, numEmployees int) {
+
 	currentMin := minTemp
+
 	currentMax := maxTemp
 
-	for i := 0; i < numEmployees; i++ {
+	for range make([]struct{}, numEmployees) {
+
 		if !scanner.Scan() {
+
 			return
+
 		}
 
 		line := strings.TrimSpace(scanner.Text())
+
 		parts := strings.Split(line, " ")
+
 		if len(parts) != expectedParts {
+
 			fmt.Println(-1)
 
 			continue
+
 		}
 
 		operator := parts[0]
-		value, err := strconv.Atoi(parts[1])
-		if err != nil {
+
+		valueStr := parts[1]
+
+		value, convErr := strconv.Atoi(valueStr)
+
+		if convErr != nil {
+
 			fmt.Println(-1)
 
 			continue
+
 		}
 
 		switch operator {
+
 		case ">=":
+
 			if value > currentMin {
+
 				currentMin = value
+
 			}
+
 		case "<=":
+
 			if value < currentMax {
+
 				currentMax = value
+
 			}
+
 		default:
+
 			fmt.Println(-1)
 
 			continue
+
 		}
 
 		if currentMin <= currentMax {
+
 			fmt.Println(currentMin)
+
 		} else {
+
 			fmt.Println(-1)
+
 		}
+
 	}
+
 }
 
 func main() {
+
 	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Split(bufio.ScanLines)
+
 	if !scanner.Scan() {
+
 		return
+
 	}
 
-	numDepartments, err := strconv.Atoi(strings.TrimSpace(scanner.Text()))
-	if err != nil {
+	numDepartmentsStr := strings.TrimSpace(scanner.Text())
+
+	numDepartmentsInt, convErr := strconv.Atoi(numDepartmentsStr)
+
+	if convErr != nil {
+
 		return
+
 	}
+
+	numDepartments := numDepartmentsInt
 
 	if numDepartments < 1 || numDepartments > 1000 {
+
 		return
+
 	}
 
-	for d := 0; d < numDepartments; d++ {
+	for range make([]struct{}, numDepartments) {
+
 		if !scanner.Scan() {
+
 			return
+
 		}
 
-		numEmployees, err := strconv.Atoi(strings.TrimSpace(scanner.Text()))
-		if err != nil {
+		numEmployeesStr := strings.TrimSpace(scanner.Text())
+
+		numEmployeesInt, convErr := strconv.Atoi(numEmployeesStr)
+
+		if convErr != nil {
+
 			return
+
 		}
+
+		numEmployees := numEmployeesInt
 
 		if numEmployees < 1 || numEmployees > 1000 {
+
 			return
+
 		}
 
 		processDepartment(scanner, numEmployees)
+
 	}
+
 }
