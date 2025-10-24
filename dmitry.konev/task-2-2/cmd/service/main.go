284e070b9dcc62c02b5ec51752c1e78a418c51d8
Konev Dmitri <konevdmitrij791@gmail.com>
2025-10-24 03:54:06 +0300

    [TASK-2-2] Fix lint issues

--- dmitry.konev/task-2-2/cmd/service/main.go
+++ dmitry.konev/task-2-2/cmd/service/main.go
@@ -7,20 +7,25 @@ import (
 
 type MinHeap []int
 
-func (h MinHeap) Len() int {
-	return len(h)
+func (h *MinHeap) Len() int {
+	return len(*h)
 }
 
-func (h MinHeap) Less(i, j int) bool {
-	return h[i] < h[j]
+func (h *MinHeap) Less(i, j int) bool {
+	return (*h)[i] < (*h)[j]
 }
 
-func (h MinHeap) Swap(i, j int) {
-	h[i], h[j] = h[j], h[i]
+func (h *MinHeap) Swap(i, j int) {
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
 func (h *MinHeap) Push(x interface{}) {
-	*h = append(*h, x.(int))
+	xInt, ok := x.(int)
+	if !ok {
+		return
+	}
+
+	*h = append(*h, xInt)
 }
 
 func (h *MinHeap) Pop() interface{} {
@@ -28,31 +33,38 @@ func (h *MinHeap) Pop() interface{} {
 	n := len(old)
 	val := old[n-1]
 	*h = old[0 : n-1]
+
 	return val
 }
 
 func main() {
-	var n, k int
-	fmt.Scan(&n)
-
-	a := make([]int, n)
-	for i := 0; i < n; i++ {
-		fmt.Scan(&a[i])
+	var n, kTop int
+	if _, err := fmt.Scan(&n); err != nil {
+		return
 	}
 
-	fmt.Scan(&k)
-
-	h := &MinHeap{}
-	heap.Init(h)
-
-	for _, val := range a {
-		if h.Len() < k {
-			heap.Push(h, val)
-		} else if val > (*h)[0] {
-			heap.Pop(h)
-			heap.Push(h, val)
+	arr := make([]int, n)
+	for i := range arr {
+		if _, err := fmt.Scan(&arr[i]); err != nil {
+			return
 		}
 	}
 
-	fmt.Println((*h)[0])
+	if _, err := fmt.Scan(&kTop); err != nil {
+		return
+	}
+
+	minHeap := &MinHeap{}
+	heap.Init(minHeap)
+
+	for _, val := range arr {
+		if minHeap.Len() < kTop {
+			heap.Push(minHeap, val)
+		} else if val > (*minHeap)[0] {
+			heap.Pop(minHeap)
+			heap.Push(minHeap, val)
+		}
+	}
+
+	fmt.Println((*minHeap)[0])
 }
