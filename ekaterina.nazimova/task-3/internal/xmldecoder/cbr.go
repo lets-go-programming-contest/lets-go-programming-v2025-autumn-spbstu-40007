8386ef7fe68b382156e89254362aa63da1e98c56
UwUshkin <ekaterinanazimova07@gmail.com>
2025-10-25 23:54:21 +0300

    [TASK-3] Fix decoding

--- ekaterina.nazimova/task-3/internal/xmldecoder/cbr.go
+++ ekaterina.nazimova/task-3/internal/xmldecoder/cbr.go
@@ -2,10 +2,30 @@ package xmldecoder
 
 import (
 	"encoding/xml"
+	"io"
 	"os"
 
 	"github.com/UwUshkin/task-3/internal/data"
+	"golang.org/x/text/encoding/charmap"
 )
+
+func decodeXMLFromReader(reader io.Reader) (*data.ValCurs, error) {
+	decoder := xml.NewDecoder(reader)
+
+	decoder.CharsetReader = func(charset string, input io.Reader) (io.Reader, error) {
+		if charset == "windows-1251" {
+			return charmap.Windows1251.NewDecoder().Reader(input), nil
+		}
+		return nil, nil
+	}
+
+	var valCurs data.ValCurs
+	if err := decoder.Decode(&valCurs); err != nil {
+		return nil, err
+	}
+
+	return &valCurs, nil
+}
 
 func DecodeCBRXML(filePath string) (*data.ValCurs, error) {
 	xmlFile, err := os.Open(filePath)
@@ -14,12 +34,5 @@ func DecodeCBRXML(filePath string) (*data.ValCurs, error) {
 	}
 	defer xmlFile.Close()
 
-	decoder := xml.NewDecoder(xmlFile)
-	var valCurs data.ValCurs
-
-	if err := decoder.Decode(&valCurs); err != nil {
-		return nil, err
-	}
-
-	return &valCurs, nil
+	return decodeXMLFromReader(xmlFile)
 }
