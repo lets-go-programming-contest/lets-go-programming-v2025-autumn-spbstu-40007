250fe7114c90dbeb7151182e3cf02f470445c217
Amirkhan Myasoutov <>
2025-10-25 12:30:43 +0300

    [TASK-2-2] Lint fix 13

--- amirkhan.myasoutov/task-2-2/cmd/service/main.go
+++ amirkhan.myasoutov/task-2-2/cmd/service/main.go
@@ -16,6 +16,7 @@ func (h *DishRating) Push(x any) {
 	if !ok {
 		return
 	}
+
 	*h = append(*h, value)
 }
 
@@ -24,67 +25,55 @@ func (h *DishRating) Pop() any {
 	n := len(old)
 	x := old[n-1]
 	*h = old[0 : n-1]
+
 	return x
 }
 
-func readDishAmount() (int, error) {
+func main() {
 	var dishAmount int
-	if _, err := fmt.Scanln(&dishAmount); err != nil {
-		return 0, fmt.Errorf("Invalid number of dishes")
+	_, err := fmt.Scanln(&dishAmount)
+	
+	if err != nil {
+		fmt.Println("Invalid number of dishes")
+
+		return
 	}
 
 	if dishAmount < 1 || dishAmount > 10000 {
-		return 0, fmt.Errorf("Invalid number of dishes")
+		fmt.Println("Invalid number of dishes")
+
+		return
 	}
 
-	return dishAmount, nil
-}
-
-func readDishRatings(dishAmount int) ([]int, error) {
 	dishQueue := make([]int, dishAmount)
 	for index := range dishQueue {
-		var rating int
-		if _, err := fmt.Scan(&rating); err != nil {
-			return nil, fmt.Errorf("Invalid dish rating")
+		_, err = fmt.Scan(&dishQueue[index])
+
+		if err != nil {
+			fmt.Println("Invalid dish rating")
+
+			return
 		}
 
-		if rating < -10000 || rating > 10000 {
-			return nil, fmt.Errorf("Invalid dish rating")
+		if dishQueue[index] < -10000 || dishQueue[index] > 10000 {
+			fmt.Println("Invalid dish rating")
+
+			return
 		}
-		dishQueue[index] = rating
 	}
-	return dishQueue, nil
-}
 
-func readPreferenceNumber(dishAmount int) (int, error) {
 	var preferenceNumber int
-	if _, err := fmt.Scan(&preferenceNumber); err != nil {
-		return 0, fmt.Errorf("Invalid number of preference")
+	_, err = fmt.Scan(&preferenceNumber)
+	
+	if err != nil {
+		fmt.Println("Invalid number of preference")
+
+		return
 	}
 
 	if preferenceNumber < 0 || preferenceNumber > dishAmount {
-		return 0, fmt.Errorf("Invalid number of preference")
-	}
+		fmt.Println("Invalid number of preference")
 
-	return preferenceNumber, nil
-}
-
-func main() {
-	dishAmount, err := readDishAmount()
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	dishQueue, err := readDishRatings(dishAmount)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	preferenceNumber, err := readPreferenceNumber(dishAmount)
-	if err != nil {
-		fmt.Println(err.Error())
 		return
 	}
 
@@ -94,18 +83,14 @@ func main() {
 	for _, value := range dishQueue {
 		if dishRating.Len() < preferenceNumber {
 			heap.Push(dishRating, value)
+
 			continue
 		}
 
-		if dishRating.Len() > 0 && (*dishRating)[0] < value {
+		if (*dishRating)[0] < value {
 			heap.Pop(dishRating)
 			heap.Push(dishRating, value)
 		}
-	}
-
-	if dishRating.Len() == 0 {
-		fmt.Println(0)
-		return
 	}
 
 	result := (*dishRating)[0]
