581b7af43d430fa8eed7190a34ace5745ada316b
Omifes <russ.out.338@gmail.com>
2025-10-28 16:58:19 +0300

    [TASK-3] fixin lint and test

--- nikita.kryzhanovskij/task-3/internal/config/config.go
+++ nikita.kryzhanovskij/task-3/internal/config/config.go
@@ -14,12 +14,15 @@ var ErrInvalidConfig = errors.New("invalid config: input-file and output-file ar
 func Load(path string) (*models.Config, error) {
 	data, err := os.ReadFile(path)
 	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil, fmt.Errorf("no such file or directory: %w", err)
+		}
 		return nil, fmt.Errorf("failed to read config file: %w", err)
 	}
 
 	var cfg models.Config
 	if err := yaml.Unmarshal(data, &cfg); err != nil {
-		return nil, fmt.Errorf("failed to parse YAML: %w", err)
+		return nil, err
 	}
 
 	if cfg.InputFile == "" || cfg.OutputFile == "" {
