8d405c0cdf24461c3d01dbfff2dabd113e7cccba
EvgeniiMilo <j.miloradov2006@gmail.com>
2025-10-31 22:45:25 +0300

    [TASK-3]

---
+++ evgenii.miloradov/task-3/cmd/service/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"sort"
+
+	"task-3/internal/config"
+	"task-3/internal/data"
+)
+
+func main() {
+	configFile := flag.String("config", "", "Path to configuration file")
+	flag.Parse()
+
+	if *configFile == "" {
+		fmt.Fprintln(os.Stderr, "Configuration file path is required")
+		os.Exit(1)
+	}
+
+	appConfig, configErr := config.Load(*configFile)
+	if configErr != nil {
+		fmt.Fprintln(os.Stderr, configErr)
+		os.Exit(1)
+	}
+
+	currencyData, dataErr := data.LoadFromFile(appConfig.InputFile)
+	if dataErr != nil {
+		fmt.Fprintln(os.Stderr, "Failed to load currency data:", dataErr)
+		os.Exit(1)
+	}
+
+	sort.Slice(currencyData.Currencies, func(i, j int) bool {
+		return currencyData.Currencies[i].Value > currencyData.Currencies[j].Value
+	})
+
+	if saveErr := currencyData.ExportToFile(appConfig.OutputFile); saveErr != nil {
+		fmt.Fprintln(os.Stderr, "Failed to save output file:", saveErr)
+		os.Exit(1)
+	}
+}
---
+++ evgenii.miloradov/task-3/go.mod
@@ -0,0 +1,9 @@
+module task-3
+
+go 1.22
+
+replace golang.org/x/net => golang.org/x/net v0.22.0
+
+require golang.org/x/net v0.22.0
+
+require golang.org/x/text v0.14.0 // indirect
