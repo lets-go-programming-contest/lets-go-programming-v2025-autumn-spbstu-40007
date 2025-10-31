ac556b9cbdb7b5621fa894f36a534d2b0d73370b
Georgy Chetverikov <blessedcakee@outlook.com>
2025-10-31 03:07:42 +0300

    [TASK-3] ...

--- georgy.chetverikov/task-3/iternal/data/data.go
+++ georgy.chetverikov/task-3/iternal/data/data.go
@@ -40,9 +40,15 @@ func (f *customFloat) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement)
 	return nil
 }
 
-func (v Valutes) Len() int           { return len(v) }
-func (v Valutes) Swap(i, j int)      { v[i], v[j] = v[j], v[i] }
-func (v Valutes) Less(i, j int) bool { return v[i].NumCode < v[j].NumCode }
+func (v Valutes) Len() int      { return len(v) }
+func (v Valutes) Swap(i, j int) { v[i], v[j] = v[j], v[i] }
+
+func (v Valutes) Less(i, j int) bool {
+	if v[i].Value != v[j].Value {
+		return v[i].Value > v[j].Value
+	}
+	return v[i].NumCode < v[j].NumCode
+}
 
 func ParseXML(data []byte) (Valutes, error) {
 	decoder := xml.NewDecoder(strings.NewReader(string(data)))
