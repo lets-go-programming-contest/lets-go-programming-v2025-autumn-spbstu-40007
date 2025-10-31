f2fd5d622197c349154fe9fc2d5b6024a923aec7
Konev Dmitri <konevdmitrij791@gmail.com>
2025-11-01 01:09:15 +0300

    [TASK-3] Fix error

--- dmitry.konev/task-3/cmd/service/main.go
+++ dmitry.konev/task-3/cmd/service/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"encoding/xml"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -39,6 +40,8 @@ type CurrencyOutput struct {
 	Value    float64 `json:"value"`
 }
 
+var errUnsupportedCharset = errors.New("unsupported charset")
+
 func loadConfig(configPath string) (Config, error) {
 	configData, err := os.ReadFile(configPath)
 	if err != nil {
@@ -62,22 +65,80 @@ func parseCurrencies(valCurs ValCurs) ([]CurrencyOutput, error) {
 	currencies := make([]CurrencyOutput, 0, len(valCurs.Valutes))
 	for _, valute := range valCurs.Valutes {
 		numCode := 0
+
 		if valute.NumCode != "" {
 			if parsed, err := strconv.Atoi(valute.NumCode); err == nil {
 				numCode = parsed
 			}
 		}
+
 		value, err := strconv.ParseFloat(strings.Replace(valute.Value, ",", ".", 1), 64)
 		if err != nil {
 			return nil, fmt.Errorf("invalid value for %s: %w", valute.CharCode, err)
 		}
+
 		currencies = append(currencies, CurrencyOutput{
 			NumCode:  numCode,
 			CharCode: valute.CharCode,
 			Value:    value,
 		})
 	}
+
 	return currencies, nil
+}
+
+func writeOutputFile(currencies []CurrencyOutput, outputDir, outputFile string) error {
+	outputFilePath := filepath.Join(outputDir, outputFile)
+
+	err := os.MkdirAll(filepath.Dir(outputFilePath), 0755)
+	if err != nil {
+		return fmt.Errorf("failed to create output directory: %w", err)
+	}
+
+	outputData, err := json.MarshalIndent(currencies, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to marshal JSON: %w", err)
+	}
+
+	err = os.WriteFile(outputFilePath, outputData, 0600)
+	if err != nil {
+		return fmt.Errorf("failed to write output file: %w", err)
+	}
+
+	return nil
+}
+
+func openFile(inputFile string) (*os.File, error) {
+	file, err := os.Open(inputFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open input file: %w", err)
+	}
+	return file, nil
+}
+
+func decodeXML(file *os.File) (ValCurs, error) {
+	decoder := xml.NewDecoder(file)
+	decoder.CharsetReader = func(charset string, input io.Reader) (io.Reader, error) {
+		switch charset {
+		case "windows-1251":
+			return charmap.Windows1251.NewDecoder().Reader(input), nil
+		default:
+			return nil, fmt.Errorf("%w: %s", errUnsupportedCharset, charset)
+		}
+	}
+
+	var valCurs ValCurs
+	err := decoder.Decode(&valCurs)
+	if err != nil {
+		return ValCurs{}, fmt.Errorf("failed to parse XML: %w", err)
+	}
+	return valCurs, nil
+}
+
+func sortCurrencies(currencies []CurrencyOutput) {
+	sort.Slice(currencies, func(i, j int) bool {
+		return currencies[i].Value > currencies[j].Value
+	})
 }
 
 func main() {
@@ -93,30 +154,20 @@ func main() {
 		panic(err)
 	}
 
-	file, err := os.Open(config.InputFile)
+	file, err := openFile(config.InputFile)
 	if err != nil {
-		panic(fmt.Sprintf("Failed to open input file: %v", err))
+		panic(err)
 	}
+
 	defer func() {
 		if closeErr := file.Close(); closeErr != nil {
 			fmt.Printf("Warning: failed to close file: %v\n", closeErr)
 		}
 	}()
 
-	decoder := xml.NewDecoder(file)
-	decoder.CharsetReader = func(charset string, input io.Reader) (io.Reader, error) {
-		switch charset {
-		case "windows-1251":
-			return charmap.Windows1251.NewDecoder().Reader(input), nil
-		default:
-			return nil, fmt.Errorf("unsupported charset: %s", charset)
-		}
-	}
-
-	var valCurs ValCurs
-	err = decoder.Decode(&valCurs)
+	valCurs, err := decodeXML(file)
 	if err != nil {
-		panic(fmt.Sprintf("Failed to parse XML: %v", err))
+		panic(err)
 	}
 
 	currencies, err := parseCurrencies(valCurs)
@@ -124,26 +175,12 @@ func main() {
 		panic(err)
 	}
 
-	sort.Slice(currencies, func(i, j int) bool {
-		return currencies[i].Value > currencies[j].Value
-	})
+	sortCurrencies(currencies)
 
-	err = os.MkdirAll(config.OutputDir, 0755)
+	err = writeOutputFile(currencies, config.OutputDir, config.OutputFile)
 	if err != nil {
-		panic(fmt.Sprintf("Failed to create output directory: %v", err))
+		panic(err)
 	}
 
-	outputFilePath := filepath.Join(config.OutputDir, config.OutputFile)
-
-	outputData, err := json.MarshalIndent(currencies, "", "  ")
-	if err != nil {
-		panic(fmt.Sprintf("Failed to marshal JSON: %v", err))
-	}
-
-	err = os.WriteFile(outputFilePath, outputData, 0600)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to write output file: %v", err))
-	}
-
-	fmt.Println("Processing completed successfully! Output saved to:", outputFilePath)
+	fmt.Println("Processing completed successfully! Output saved to:", filepath.Join(config.OutputDir, config.OutputFile))
 }
