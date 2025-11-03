ac1c8a1138a3fd0f1dec85336ef293f59825549e
bdshka <bokovad2@gmail.com>
2025-11-03 03:19:24 +0300

    [TASK-3]  (#111)
    * [TASK-3] Initial commit
    
    * [TASK-3] Initial commit
    
    * [TASK-3] Initial commit
    
    * [TASK-3] Initial commit
    
    * [TASK-3] Initial commit
    
    * [TASK-3] Initial commit 2
    
    * [TASK-3] Initial commit 3
    
    * [TASK-3]
    
    * [TASK-3]
    
    * [TASK-3]
    
    * [TASK-3]
    
    * [TASK-3]
    
    * [TASK-3]
    
    * [TASK-3] lint fix
    
    * [TASK-3] lint fix
    
    * [TASK-3] lint fix
    
    * [TASK-3] lint fix
    
    * [TASK-3] lint fix
    
    * [TASK-3] lint fix
    
    * [TASK-3] lint fix
    
    * [TASK-3] lint fix
    
    * [TASK-3] lint fix
    
    * [TASK-3] lint fix
    
    * [TASK-3] lint fix
    
    * [TASK-3] lint fix
    
    * [TASK-3] lint fix

---
+++ daria.bokova/task-3/internal/data/data.go
@@ -0,0 +1,159 @@
+package data
+
+import (
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+
+	"golang.org/x/text/encoding/charmap"
+)
+
+var ErrUnsupportedCharset = errors.New("неподдерживаемая кодировка")
+
+type CurrencyList struct {
+	XMLName xml.Name       `xml:"ValCurs"`
+	Items   []CurrencyItem `xml:"Valute"`
+}
+
+type CurrencyItem struct {
+	XMLName      xml.Name `xml:"Valute"`
+	NumberCode   string   `xml:"NumCode"`
+	CurrencyCode string   `xml:"CharCode"`
+	RawValue     string   `xml:"Value"`
+	NumericValue float64  `xml:"-"`
+}
+
+type ProcessedCurrency struct {
+	NumberCode   int     `json:"num_code"  xml:"NumCode"  yaml:"num_code"`
+	CurrencyCode string  `json:"char_code" xml:"CharCode" yaml:"char_code"`
+	NumericValue float64 `json:"value"     xml:"Value"    yaml:"value"`
+}
+
+type ProcessedCurrencyList struct {
+	XMLName xml.Name            `xml:"ValCurs"`
+	Items   []ProcessedCurrency `xml:"Valute"`
+}
+
+type CurrencySorter []CurrencyItem
+
+func (cs CurrencySorter) Count() int            { return len(cs) }
+func (cs CurrencySorter) Exchange(i, j int)     { cs[i], cs[j] = cs[j], cs[i] }
+func (cs CurrencySorter) Compare(i, j int) bool { return cs[i].NumericValue > cs[j].NumericValue }
+
+func ProcessXMLFile(filename string) []CurrencyItem {
+	fileContent, readErr := readXMLFile(filename)
+	if readErr != nil {
+		handleFileReadError(filename, readErr)
+	}
+
+	currencyData, decodeErr := parseXMLData(fileContent)
+	if decodeErr != nil {
+		handleXMLParseError(decodeErr)
+	}
+
+	return processCurrencyValues(currencyData.Items)
+}
+
+func readXMLFile(filename string) ([]byte, error) {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка чтения файла '%s': %w", filename, err)
+	}
+
+	return content, nil
+}
+
+func parseXMLData(content []byte) (CurrencyList, error) {
+	xmlReader := strings.NewReader(string(content))
+	parser := xml.NewDecoder(xmlReader)
+
+	parser.CharsetReader = createCharsetConverter()
+
+	var currencyData CurrencyList
+	if err := parser.Decode(&currencyData); err != nil {
+		return CurrencyList{}, fmt.Errorf("ошибка декодирования XML: %w", err)
+	}
+
+	return currencyData, nil
+}
+
+func createCharsetConverter() func(string, io.Reader) (io.Reader, error) {
+	return func(encoding string, reader io.Reader) (io.Reader, error) {
+		if encoding == "windows-1251" {
+			return charmap.Windows1251.NewDecoder().Reader(reader), nil
+		}
+
+		return nil, fmt.Errorf("%w: %s", ErrUnsupportedCharset, encoding)
+	}
+}
+
+func processCurrencyValues(items []CurrencyItem) []CurrencyItem {
+	processedItems := make([]CurrencyItem, 0, len(items))
+
+	for _, item := range items {
+		processedItem := convertCurrencyValue(item)
+		processedItems = append(processedItems, processedItem)
+	}
+
+	return processedItems
+}
+
+func convertCurrencyValue(item CurrencyItem) CurrencyItem {
+	normalizedValue := strings.ReplaceAll(item.RawValue, ",", ".")
+
+	convertedValue, err := strconv.ParseFloat(normalizedValue, 64)
+	if err != nil {
+		handleValueConversionError(item.RawValue, err)
+	}
+
+	item.NumericValue = convertedValue
+
+	return item
+}
+
+func (item CurrencyItem) ConvertToOutputFormat() ProcessedCurrency {
+	codeValue := convertNumberCode(item.NumberCode)
+
+	return ProcessedCurrency{
+		NumberCode:   codeValue,
+		CurrencyCode: item.CurrencyCode,
+		NumericValue: item.NumericValue,
+	}
+}
+
+func convertNumberCode(code string) int {
+	if code == "" {
+		return 0
+	}
+
+	convertedCode, err := strconv.Atoi(code)
+	if err != nil {
+		handleCodeConversionError(code, err)
+	}
+
+	return convertedCode
+}
+
+func handleFileReadError(filename string, err error) {
+	fmt.Printf("Ошибка чтения исходного файла '%s': %v\n", filename, err)
+	panic(fmt.Errorf("невозможно прочитать исходный XML файл: %w", err))
+}
+
+func handleXMLParseError(err error) {
+	fmt.Printf("Ошибка декодирования XML данных: %v\n", err)
+	panic(fmt.Errorf("ошибка обработки XML данных: %w", err))
+}
+
+func handleValueConversionError(value string, err error) {
+	fmt.Printf("Ошибка преобразования значения '%s' в число: %v\n", value, err)
+	panic(fmt.Errorf("некорректное значение валюты: %s: %w", value, err))
+}
+
+func handleCodeConversionError(code string, err error) {
+	fmt.Printf("Ошибка преобразования NumCode '%s' в целое число: %v\n", code, err)
+	panic(fmt.Errorf("некорректный NumCode: %s: %w", code, err))
+}
---
+++ daria.bokova/task-3/internal/output/output.go
@@ -0,0 +1,115 @@
+package output
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/bdshka/task-3/internal/data"
+	"gopkg.in/yaml.v3"
+)
+
+var ErrFormatNotSupported = errors.New("неподдерживаемый формат вывода")
+
+type DataExporter struct{}
+
+func ExportData(filename string, formatType string, currencies []data.ProcessedCurrency) {
+	outputData, encodeErr := encodeToFormat(formatType, currencies)
+	if encodeErr != nil {
+		handleEncodingError(formatType, encodeErr)
+	}
+
+	dirErr := createOutputDirectory(filename)
+	if dirErr != nil {
+		handleDirectoryError(filepath.Dir(filename), dirErr)
+	}
+
+	writeErr := writeToFile(filename, outputData)
+	if writeErr != nil {
+		handleFileWriteError(filename, writeErr)
+	}
+}
+
+func encodeToFormat(format string, currencies []data.ProcessedCurrency) ([]byte, error) {
+	format = strings.ToLower(format)
+
+	switch format {
+	case "json":
+		return encodeToJSON(currencies)
+	case "yaml":
+		return encodeToYAML(currencies)
+	case "xml":
+		return encodeToXML(currencies)
+	default:
+		return nil, fmt.Errorf("%w: %s. Доступные: json, yaml, xml",
+			ErrFormatNotSupported, format)
+	}
+}
+
+func encodeToJSON(currencies []data.ProcessedCurrency) ([]byte, error) {
+	jsonData, err := json.MarshalIndent(currencies, "", "  ")
+	if err != nil {
+		return nil, fmt.Errorf("ошибка кодирования JSON: %w", err)
+	}
+
+	return jsonData, nil
+}
+
+func encodeToYAML(currencies []data.ProcessedCurrency) ([]byte, error) {
+	yamlData, err := yaml.Marshal(currencies)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка кодирования YAML: %w", err)
+	}
+
+	return yamlData, nil
+}
+
+func encodeToXML(currencies []data.ProcessedCurrency) ([]byte, error) {
+	xmlData := data.ProcessedCurrencyList{
+		XMLName: xml.Name{Space: "", Local: "ValCurs"},
+		Items:   currencies,
+	}
+
+	xmlBytes, err := xml.MarshalIndent(xmlData, "", "  ")
+	if err != nil {
+		return nil, fmt.Errorf("ошибка кодирования XML: %w", err)
+	}
+
+	return []byte(xml.Header + string(xmlBytes)), nil
+}
+
+func createOutputDirectory(filePath string) error {
+	directory := filepath.Dir(filePath)
+	if err := os.MkdirAll(directory, 0o755); err != nil { //nolint:mnd
+		return fmt.Errorf("невозможно создать директорию: %w", err)
+	}
+
+	return nil
+}
+
+func writeToFile(filePath string, data []byte) error {
+	if err := os.WriteFile(filePath, data, 0o600); err != nil { //nolint:mnd
+		return fmt.Errorf("ошибка записи в файл: %w", err)
+	}
+
+	return nil
+}
+
+func handleEncodingError(format string, err error) {
+	fmt.Printf("Ошибка кодирования данных в формат '%s': %v\n", format, err)
+	panic(fmt.Errorf("сбой преобразования данных: %w", err))
+}
+
+func handleDirectoryError(dirPath string, err error) {
+	fmt.Printf("Ошибка создания директории '%s': %v\n", dirPath, err)
+	panic(fmt.Errorf("невозможно создать директорию: %w", err))
+}
+
+func handleFileWriteError(filePath string, err error) {
+	fmt.Printf("Ошибка записи в файл '%s': %v\n", filePath, err)
+	panic(fmt.Errorf("сбой сохранения файла: %w", err))
+}
