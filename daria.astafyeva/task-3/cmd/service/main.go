c7872506f4ee7a48229601162473f7db701c48cc
itsdasha <m.astafeva07081985@gmail.com>
2025-11-03 04:32:10 +0300

    [TASK-3]  (#124)
    * [TASK-3] initil commit
    
    * [TASK-3] ...
    
    * [TASK-3] fix lint
    
    * [TASK-3] fix lint
    
    * [TASK-3] fix lint
    
    * [TASK-3] fix lint
    
    * [TASK-3] fix lint
    
    * [TASK-3] fix lint
    
    * [TASK-3] fix lint
    
    * [TASK-3] fix lint

---
+++ daria.astafyeva/task-3/cmd/service/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"sort"
+	"strconv"
+
+	"github.com/itsdasha/task-3/internal/config"
+	"github.com/itsdasha/task-3/internal/data"
+	"github.com/itsdasha/task-3/internal/output"
+)
+
+type CurrencyProcessor struct {
+	Raw    []data.Currency
+	Result []data.OutputCurrency
+}
+
+func main() {
+	var cfgPath, fmtType string
+
+	flag.StringVar(&cfgPath, "config", "config.yaml", "Path to YAML config")
+	flag.StringVar(&fmtType, "output-format", "json", "Output format: json, yaml, xml")
+	flag.Parse()
+
+	settings := config.LoadSettings(cfgPath)
+
+	processor := &CurrencyProcessor{
+		Raw:    make([]data.Currency, 0),
+		Result: make([]data.OutputCurrency, 0),
+	}
+	processor.Raw = data.LoadCurrencies(settings.SourceFile)
+
+	sort.Slice(processor.Raw, func(i, j int) bool {
+		return processor.Raw[i].Rate > processor.Raw[j].Rate
+	})
+
+	processor.Convert()
+	output.Save(processor.Result, settings.TargetFile, fmtType)
+
+	fmt.Printf("Processed %d currencies -> '%s' [%s]\n",
+		len(processor.Result), settings.TargetFile, fmtType)
+}
+
+func (p *CurrencyProcessor) Convert() {
+	for _, currency := range p.Raw {
+		num := 0
+
+		if currency.NumCode != "" {
+			val, err := strconv.Atoi(currency.NumCode)
+			if err != nil {
+				panic(fmt.Errorf("invalid NumCode '%s': %w", currency.NumCode, err))
+			}
+
+			num = val
+		}
+
+		p.Result = append(p.Result, data.OutputCurrency{
+			Num:    num,
+			Char:   currency.CharCode,
+			Amount: currency.Rate,
+		})
+	}
+}
---
+++ daria.astafyeva/task-3/internal/data/data.go
@@ -0,0 +1,72 @@
+package data
+
+import (
+	"encoding/xml"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+
+	"golang.org/x/text/encoding/charmap"
+)
+
+var ErrUnknownCharset = errors.New("unknown charset")
+
+type Currency struct {
+	XMLName  xml.Name `xml:"Valute"`
+	NumCode  string   `xml:"NumCode"`
+	CharCode string   `xml:"CharCode"`
+	ValueStr string   `xml:"Value"`
+	Rate     float64  `xml:"-"`
+}
+
+type OutputCurrency struct {
+	Num    int     `json:"num_code"  xml:"NumCode"  yaml:"num_code"`
+	Char   string  `json:"char_code" xml:"CharCode" yaml:"char_code"`
+	Amount float64 `json:"value"     xml:"Value"    yaml:"value"`
+}
+
+func LoadCurrencies(path string) []Currency {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		panic(fmt.Errorf("cannot read XML file '%s': %w", path, err))
+	}
+
+	decoder := xml.NewDecoder(strings.NewReader(string(content)))
+	decoder.CharsetReader = charsetReader
+
+	var wrapper struct {
+		Currencies []Currency `xml:"Valute"`
+	}
+
+	err = decoder.Decode(&wrapper)
+	if err != nil {
+		panic(fmt.Errorf("XML decode failed: %w", err))
+	}
+
+	currencies := make([]Currency, 0, len(wrapper.Currencies))
+
+	for _, curr := range wrapper.Currencies {
+		curr.ValueStr = strings.ReplaceAll(curr.ValueStr, ",", ".")
+
+		val, err := strconv.ParseFloat(curr.ValueStr, 64)
+		if err != nil {
+			panic(fmt.Errorf("invalid value '%s': %w", curr.ValueStr, err))
+		}
+
+		curr.Rate = val
+		currencies = append(currencies, curr)
+	}
+
+	return currencies
+}
+
+func charsetReader(charset string, r io.Reader) (io.Reader, error) {
+	if strings.EqualFold(charset, "windows-1251") {
+		return charmap.Windows1251.NewDecoder().Reader(r), nil
+	}
+
+	return nil, fmt.Errorf("%w: %s", ErrUnknownCharset, charset)
+}
