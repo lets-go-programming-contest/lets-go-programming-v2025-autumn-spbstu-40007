523fb51125f3a16e488114d194bb13e95604849f
gosha <blessedcakee@outlook.com>
2025-11-03 04:26:43 +0300

    [TASK-3]  (#122)
    * [TASK-3] initial commit
    
    * [TASK-3] fixes + additional task
    
    * [TASK-3] essential changes
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] ...
    
    * [TASK-3] linter fix + did the additional task propely
    
    * [TASK-3] linter
    
    * [TASK-3] linter
    
    * [TASK-3] linter
    
    * [TASK-3] linter

---
+++ georgy.chetverikov/task-3/cmd/service/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/falsefeelings/task-3/iternal/config"
+	"github.com/falsefeelings/task-3/iternal/converter"
+	"github.com/falsefeelings/task-3/iternal/data"
+	"github.com/falsefeelings/task-3/iternal/pathmaker"
+)
+
+const filePermissions = 0o600
+
+func main() {
+	var (
+		configPath   string
+		outputFormat string
+	)
+
+	flag.StringVar(&configPath, "config", "config.yaml", "Path to config file")
+	flag.StringVar(&outputFormat, "output-format", "json", "Output format: json, yaml, xml")
+	flag.Parse()
+
+	config, err := config.Read(configPath)
+	if err != nil {
+		log.Fatal("Config error:", err)
+	}
+
+	if _, err := os.Stat(config.InputFile); os.IsNotExist(err) {
+		log.Fatalf("no such file or directory")
+	}
+
+	xmlData, err := os.ReadFile(config.InputFile)
+	if err != nil {
+		log.Fatal("Read file error:", err)
+	}
+
+	valutes, err := data.ParseXML(xmlData)
+	if err != nil {
+		log.Fatal("XML parse error:", err)
+	}
+
+	conv := converter.New()
+
+	outputData, err := conv.Convert(valutes, outputFormat)
+	if err != nil {
+		log.Fatal("Conversion error:", err)
+	}
+
+	err = pathmaker.CreateOutPath(config.OutputFile)
+	if err != nil {
+		log.Fatal("Create path error:", err)
+	}
+
+	err = os.WriteFile(config.OutputFile, outputData, filePermissions)
+	if err != nil {
+		log.Fatal("Write file error:", err)
+	}
+}
