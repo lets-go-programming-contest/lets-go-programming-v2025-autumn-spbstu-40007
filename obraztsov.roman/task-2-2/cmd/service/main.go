236fe8c339095c2e00ddc69bdd95af54eb245d78
rom1424 <5356488@gmail.com>
2025-11-03 22:20:07 +0300

    [TASK-2-2] Update maingo

--- obraztsov.roman/task-2-2/cmd/service/main.go
+++ obraztsov.roman/task-2-2/cmd/service/main.go
@@ -11,16 +11,18 @@ import (
 
 type IntHeap []int
 
-func (h IntHeap) Len() int { return len(h) }
+func (h *IntHeap) Len() int { return len(*h) }
 
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
 
-func (h IntHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Swap(i, j int) { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
 func (h *IntHeap) Push(x interface{}) {
 	value, ok := x.(int)
+
 	if !ok {
 		fmt.Println("Error use int")
+
 		return
 	}
 
@@ -37,10 +39,10 @@ func (h *IntHeap) Pop() interface{} {
 	return item
 }
 
-func (h IntHeap) Sort() {
+func (h *IntHeap) Sort() {
 	n := h.Len()
-	for i := 0; i < n-1; i++ {
-		for j := 0; j < n-1-i; j++ {
+	for i := range n - 1 {
+		for j := range n - 1 - i {
 			if h.Less(j+1, j) {
 				h.Swap(j, j+1)
 			}
@@ -54,21 +56,26 @@ func main() {
 	intHeap := &IntHeap{}
 
 	_, err1 := fmt.Scanln(&numberAll)
+
 	if err1 != nil {
 		fmt.Println("Invalid Value")
+
 		return
 	}
 
 	if numberAll < 1 || numberAll > 10000 {
 		fmt.Println("Number of food out of range")
+
 		return
 	}
 
 	reader := bufio.NewReader(os.Stdin)
 
 	line, err3 := reader.ReadString('\n')
+
 	if err3 != nil {
 		fmt.Println("Error cant read food values")
+
 		return
 	}
 
@@ -77,49 +84,51 @@ func main() {
 
 	if len(parts) != numberAll {
 		fmt.Println("Invalid input")
+
 		return
 	}
 
-	validInput := true
-
 	for _, part := range parts {
 		numberOfFood, err := strconv.Atoi(part)
+
 		if err != nil {
 			fmt.Println("Invalid value")
-			validInput = false
+
 			return
 		}
 
 		if numberOfFood < -10000 || numberOfFood > 10000 {
 			fmt.Println("Invalid value")
-			validInput = false
+
 			return
 		}
 
 		heap.Push(intHeap, numberOfFood)
 	}
 
-	if !validInput {
-		return
-	}
-
 	intHeap.Sort()
 
 	value, err4 := reader.ReadString('\n')
+
 	if err4 != nil {
 		fmt.Println("Error of value food")
+
 		return
 	}
 
 	value = strings.TrimSpace(value)
+
 	valueFood, err5 := strconv.Atoi(value)
+
 	if err5 != nil {
 		fmt.Println("Invalid value")
+
 		return
 	}
 
 	if valueFood <= 0 || valueFood > len(*intHeap) {
 		fmt.Println("Invalid value")
+
 		return
 	}
 
