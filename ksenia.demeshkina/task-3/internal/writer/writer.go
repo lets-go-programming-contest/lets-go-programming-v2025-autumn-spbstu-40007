93304b5372f2d0ccbd8fbbdc02e221fb4e332a4f
Ксеня <demeshkinakseniaa@gmail.com>
2025-11-03 04:00:23 +0300

    [TASK-3]  (#120)
    * [TASK-3] intial commit
    
    * [TASK-3] go mod tidy
    
    * [TASK-3] fix go mod
    
    * [TASK-3] fix go mod
    
    * [TASK-3] fix test
    
    * [TASK-3] fix lint
    
    * [TASK-3] fix lint second try
    
    * [TASK-3] fix lint third try
    
    * [TASK-3] again linter
    
    * [TASK-3] linter swearing again
    
    * [TASK-3] i hope this is the last attempt...
    
    * [TASK-3] maybe this time

---
+++ ksenia.demeshkina/task-3/internal/writer/writer.go
@@ -0,0 +1,50 @@
+package writer
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/ksuah/task-3/internal/xmlparser"
+)
+
+type CurrencyOutput struct {
+	NumCode  int     `json:"num_code"`
+	CharCode string  `json:"char_code"`
+	Value    float64 `json:"value"`
+}
+
+func SaveToJSON(path string, valutes []xmlparser.Valute) {
+	dir := filepath.Dir(path)
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+		log.Panicf("Failed to create directory: %v", err)
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		log.Panicf("Cannot create output file: %v", err)
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Panicf("Failed to close file: %v", err)
+		}
+	}()
+
+	output := make([]CurrencyOutput, 0, len(valutes))
+	for _, v := range valutes {
+		output = append(output, CurrencyOutput{
+			NumCode:  v.NumCode,
+			CharCode: v.CharCode,
+			Value:    v.Value,
+		})
+	}
+
+	enc := json.NewEncoder(file)
+	enc.SetIndent("", "  ")
+
+	if err := enc.Encode(output); err != nil {
+		log.Panicf("Failed to write JSON: %v", err)
+	}
+}
