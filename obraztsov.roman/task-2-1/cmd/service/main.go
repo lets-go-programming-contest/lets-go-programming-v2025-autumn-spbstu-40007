f57cbb5a0571d7ca6c7ae96ba2d8e3e01eed877a
rom1424 <5356488@gmail.com>
2025-11-04 13:55:51 +0300

    [TASK-2-1]

---
+++ obraztsov.roman/task-2-1/cmd/service/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func takeTemperature(employees int) {
+	minTemp := 15
+	maxTemp := 30
+
+	for range employees {
+
+		var (
+			operation string
+			value     string
+		)
+
+		_, err3 := fmt.Scanln(&operation, &value)
+		if err3 != nil {
+			fmt.Println("Invalid operation or value")
+			return
+		}
+
+		tempInt, err := strconv.Atoi(value)
+		if err != nil {
+			fmt.Println("Error invalid value")
+		}
+
+		switch operation {
+		case "<=":
+			maxTemp = min(maxTemp, tempInt)
+		case ">=":
+			minTemp = max(minTemp, tempInt)
+		}
+		if minTemp > maxTemp {
+			fmt.Println(-1)
+		} else {
+			fmt.Println(minTemp)
+		}
+
+	}
+}
+
+func main() {
+
+	var (
+		departments, employees int
+	)
+
+	_, err1 := fmt.Scanln(&departments)
+	if err1 != nil {
+		fmt.Println("Invalid departments")
+		return
+	}
+
+	for range departments {
+		_, err2 := fmt.Scanln(&employees)
+		if err2 != nil {
+			fmt.Println("Invalid employees")
+			return
+		}
+
+		takeTemperature(employees)
+	}
+
+}
