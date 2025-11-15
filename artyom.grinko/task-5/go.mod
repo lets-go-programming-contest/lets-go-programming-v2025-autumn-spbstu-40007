82043ac425e8950ae4d7bc84858136d0b9968276
Timothée <sainetsauf@disroot.org>
2025-11-16 01:15:20 +0300

    [TASK-5] Initial commit.

---
+++ artyom.grinko/task-5/go.mod
@@ -0,0 +1,5 @@
+module task-5
+
+go 1.24.0
+
+require golang.org/x/sync v0.18.0
---
+++ artyom.grinko/task-5/pkg/handlers/handlers.go
@@ -0,0 +1,113 @@
+package handlers
+
+import (
+	"context"
+	"fmt"
+	"strings"
+)
+
+func PrefixDecoratorFunc(
+	context context.Context,
+	input chan string,
+	output chan string,
+) error {
+	defer close(output)
+
+	for {
+		select {
+		case <-context.Done():
+			return context.Err()
+
+		case x, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(x, "no decorator") {
+				return fmt.Errorf("handlers.PrefixDecoratorFunc: can't be decorated")
+			}
+
+			if strings.HasPrefix(x, "decorated: ") {
+				output <- x
+
+				continue
+			}
+
+			select {
+			case <-context.Done():
+				return context.Err()
+
+			case output <- "decorated: " + x:
+			}
+		}
+	}
+}
+
+func MultiplexerFunc(
+	context context.Context,
+	inputs []chan string,
+	output chan string,
+) error {
+	defer close(output)
+
+	for {
+		select {
+		case <-context.Done():
+			return context.Err()
+
+		default:
+			for _, input := range inputs {
+				select {
+				case <-context.Done():
+					return context.Err()
+
+				case x, ok := <-input:
+					if !ok {
+						return nil
+					}
+
+					if !strings.Contains(x, "no multiplexer") {
+						select {
+						case <-context.Done():
+							return context.Err()
+
+						case output <- x:
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+func SeparatorFunc(
+	context context.Context,
+	input chan string,
+	outputs []chan string,
+) error {
+	defer (func() {
+		for _, output := range outputs {
+			close(output)
+		}
+	})()
+
+	for i := 0; ; {
+		select {
+		case <-context.Done():
+			return context.Err()
+
+		case x, ok := <-input:
+			if !ok {
+				return nil
+			}
+
+			select {
+			case <-context.Done():
+				return context.Err()
+
+			case outputs[i%len(outputs)] <- x:
+				i++
+			}
+		}
+	}
+}
