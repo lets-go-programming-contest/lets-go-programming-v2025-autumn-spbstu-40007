dab7626157c6e318314258bf629eb19a3a15f184
egor butakovskij <mrtntkatz@gmail.com>
2025-11-26 19:54:36 +0300

    [TASK-5] Initial commit
    [TASK-5] mod version
    
    [TASK-5] mod problems?
    
    [TASK-5] smth strange
    
    [TASK-5] dependencies change
    
    [TASK-5] test's deadlock fix
    
    [TASK-5]lint changes

---
+++ egor.butakovskij/task-5/pkg/conveyer/conveyer.go
@@ -0,0 +1,235 @@
+package conveyer
+
+import (
+	"context"
+	"errors"
+	"sync"
+)
+
+var (
+	errChanNotFound   = errors.New("chan not found")
+	ErrInvalidHandler = errors.New("invalid handler function signature")
+)
+
+type Conveyer struct {
+	channels       map[string]chan string
+	HandlerConfigs []HandlerConfig
+	bufferSize     int
+	mu             sync.Mutex
+	wg             sync.WaitGroup
+}
+
+type HandlerConfig struct {
+	Fn        interface{}
+	InputIds  []string
+	OutputIds []string
+	Type      int
+}
+
+const (
+	DecoratorType   = 1
+	MultiplexerType = 2
+	SeparatorType   = 3
+)
+
+func New(size int) *Conveyer {
+	c := &Conveyer{
+		channels:       make(map[string]chan string),
+		HandlerConfigs: make([]HandlerConfig, 0),
+		bufferSize:     size,
+		mu:             sync.Mutex{},
+		wg:             sync.WaitGroup{},
+	}
+	return c
+}
+
+func (c *Conveyer) getChan(name string) chan string {
+	if ch, exists := c.channels[name]; exists {
+		return ch
+	}
+
+	ch := make(chan string, c.bufferSize)
+	c.channels[name] = ch
+
+	return ch
+}
+
+func (c *Conveyer) RegisterDecorator(
+	fn func(
+		ctx context.Context,
+		input chan string,
+		output chan string,
+	) error,
+	input string,
+	output string,
+) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.getChan(input)
+	c.getChan(output)
+
+	config := HandlerConfig{
+		Type:      DecoratorType,
+		Fn:        fn,
+		InputIds:  []string{input},
+		OutputIds: []string{output},
+	}
+
+	c.HandlerConfigs = append(c.HandlerConfigs, config)
+}
+
+func (c *Conveyer) RegisterMultiplexer(
+	fn func(
+		ctx context.Context,
+		inputs []chan string,
+		output chan string,
+	) error,
+	inputs []string,
+	output string,
+) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for _, input := range inputs {
+		c.getChan(input)
+	}
+
+	c.getChan(output)
+
+	config := HandlerConfig{
+		Type:      MultiplexerType,
+		Fn:        fn,
+		InputIds:  inputs,
+		OutputIds: []string{output},
+	}
+
+	c.HandlerConfigs = append(c.HandlerConfigs, config)
+}
+
+func (c *Conveyer) RegisterSeparator(
+	fn func(
+		ctx context.Context,
+		input chan string,
+		outputs []chan string,
+	) error,
+	input string,
+	outputs []string,
+) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for _, output := range outputs {
+		c.getChan(output)
+	}
+
+	c.getChan(input)
+
+	config := HandlerConfig{
+		Type:      SeparatorType,
+		Fn:        fn,
+		InputIds:  []string{input},
+		OutputIds: outputs,
+	}
+
+	c.HandlerConfigs = append(c.HandlerConfigs, config)
+}
+
+func (c *Conveyer) Send(input string, data string) error {
+	c.mu.Lock()
+	ch, exists := c.channels[input]
+	c.mu.Unlock()
+	if !exists {
+		return errChanNotFound
+	}
+
+	ch <- data
+
+	return nil
+}
+
+func (c *Conveyer) Recv(output string) (string, error) {
+	c.mu.Lock()
+	ch, exists := c.channels[output]
+	c.mu.Unlock()
+	if !exists {
+		return "", errChanNotFound
+	}
+
+	data, ok := <-ch
+	if !ok {
+		return "undefined", nil
+	}
+
+	return data, nil
+}
+
+func (c *Conveyer) Run(ctx context.Context) error {
+	errChan := make(chan error, len(c.HandlerConfigs))
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	c.mu.Lock()
+
+	for _, config := range c.HandlerConfigs {
+		var (
+			inputChans  []chan string
+			outputChans []chan string
+		)
+
+		for _, id := range config.InputIds {
+			inputChans = append(inputChans, c.channels[id])
+		}
+
+		for _, id := range config.OutputIds {
+			outputChans = append(outputChans, c.channels[id])
+		}
+
+		c.wg.Add(1)
+
+		go func(cfg HandlerConfig, inputs []chan string, outputs []chan string) {
+			defer c.wg.Done()
+			var err error
+			switch cfg.Type {
+			case DecoratorType:
+
+				fn, ok := cfg.Fn.(func(context.Context, chan string, chan string) error)
+				if !ok {
+					errChan <- ErrInvalidHandler
+					return
+				}
+				err = fn(ctx, inputs[0], outputs[0])
+			case MultiplexerType:
+				fn, ok := cfg.Fn.(func(context.Context, []chan string, chan string) error)
+				if !ok {
+					errChan <- ErrInvalidHandler
+					return
+				}
+				err = fn(ctx, inputs, outputs[0])
+			case SeparatorType:
+
+				fn, ok := cfg.Fn.(func(context.Context, chan string, []chan string) error)
+				if !ok {
+					errChan <- ErrInvalidHandler
+					return
+				}
+				err = fn(ctx, inputs[0], outputs)
+			}
+
+			if err != nil {
+				errChan <- err
+			}
+		}(config, inputChans, outputChans)
+	}
+	c.mu.Unlock()
+	select {
+	case err := <-errChan:
+		cancel()
+		c.wg.Wait()
+		return err
+	case <-ctx.Done():
+		c.wg.Wait()
+		return nil
+	}
+}
---
+++ egor.butakovskij/task-5/pkg/handlers/handlers.go
@@ -0,0 +1,148 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"strings"
+	"sync"
+)
+
+var errPrefixDecoratorFuncCantBeDecorated = errors.New("can't be decorated")
+
+func readInputToTransfer(ctx context.Context, input chan string, transfer chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case data, ok := <-input:
+			if !ok {
+				return
+			}
+
+			select {
+			case transfer <- data:
+			case <-ctx.Done():
+				return
+			}
+		}
+	}
+}
+
+func PrefixDecoratorFunc(
+	ctx context.Context,
+	input chan string,
+	output chan string,
+) error {
+	defer close(output)
+	prefix := "decorated: "
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+			if strings.Contains(data, "no decorator") {
+				return errPrefixDecoratorFuncCantBeDecorated
+			}
+
+			processedData := data
+			if !strings.HasPrefix(data, prefix) {
+				processedData = prefix + data
+			}
+
+			select {
+			case output <- processedData:
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	}
+}
+
+func MultiplexerFunc(
+	ctx context.Context,
+	inputs []chan string,
+	output chan string,
+) error {
+	var wg sync.WaitGroup
+
+	transfer := make(chan string)
+
+	defer close(output)
+
+	for _, input := range inputs {
+		wg.Add(1)
+		go readInputToTransfer(ctx, input, transfer, &wg)
+	}
+	go func() {
+		wg.Wait()
+
+		close(transfer)
+	}()
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+
+		case data, ok := <-transfer:
+			if !ok {
+				return nil
+			}
+
+			if strings.Contains(data, "no multiplexer") {
+				continue
+			}
+
+			select {
+			case output <- data:
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	}
+}
+
+func SeparatorFunc(
+	ctx context.Context,
+	input chan string,
+	outputs []chan string,
+) error {
+	defer (func() {
+		for _, output := range outputs {
+			defer func() {
+				if r := recover(); r != nil {
+				}
+			}()
+			close(output)
+		}
+	})()
+
+	var counter int
+	if len(outputs) == 0 {
+		return nil
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, ok := <-input:
+			if !ok {
+				return nil
+			}
+			targetIndex := counter % len(outputs)
+			targetChan := outputs[targetIndex]
+
+			select {
+			case targetChan <- data:
+				counter++
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	}
+}
