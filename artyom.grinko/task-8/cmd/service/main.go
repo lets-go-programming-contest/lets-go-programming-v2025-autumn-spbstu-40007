a09dad8642b396bcf8437e116611040a422d112c
Timothée <sainetsauf@disroot.org>
2025-12-05 08:00:54 +0300

    [TASK-8] Initial commit.

---
+++ artyom.grinko/task-8/cmd/service/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"task-8/pkg/config"
+)
+
+func main() {
+    config := config.New()
+
+    fmt.Println(config.Environment, config.LogLevel)
+}
---
+++ artyom.grinko/task-8/pkg/config/config.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+    yaml "github.com/goccy/go-yaml"
+)
+
+type Config struct {
+    Environment string
+    LogLevel string `yaml:"log_level
+}
+
+func New() *Config {
+    config := &Config{}
+    if err := yaml.Unmarshal([]byte(configContents), config); err != nil {
+        panic(err)
+    }
+    
+    return config
+}
