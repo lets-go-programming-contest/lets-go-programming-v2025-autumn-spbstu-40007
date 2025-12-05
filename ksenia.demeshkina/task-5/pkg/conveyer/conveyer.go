6c93994febf0eb39202c81a361951160df4f9701
Ксения Демешкина <demeshkinakseniaa@gmail.com>
2025-12-06 01:50:46 +0300

    [TASK-5] fix test

--- ksenia.demeshkina/task-5/pkg/conveyer/conveyer.go
+++ ksenia.demeshkina/task-5/pkg/conveyer/conveyer.go
@@ -16,52 +16,46 @@ const (
 )
 
 type step struct {
-	tp   StepType
-	name string
-
-	in     string
-	out    string
-	outOk  string
-	outErr string
-
-	dec handlers.DecoratingHandler
-	sep handlers.SeparatingHandler
-	mux handlers.MultiplexingHandler
+	tp       StepType
+	name     string
+	input    string
+	inputs   []string
+	outputs  []string
+	dec      handlers.DecoratingHandler
+	sep      handlers.SeparatingHandler
+	mux      handlers.MultiplexingHandler
 }
 
 type Conveyer struct {
-	ctx    context.Context
-	cancel context.CancelFunc
-
-	steps []step
-
-	chans map[string]chan string
+	steps   []step
+	ctx     context.Context
+	cancel  context.CancelFunc
+	inputs  map[string]chan string
+	outputs map[string]chan string
 }
 
-func New(_ int) *Conveyer {
+func New(names ...string) *Conveyer {
+	inputs := make(map[string]chan string)
+	for _, n := range names {
+		inputs[n] = make(chan string)
+	}
 	return &Conveyer{
-		chans: make(map[string]chan string),
+		inputs:  inputs,
+		outputs: make(map[string]chan string),
 	}
 }
 
-// Создаём канал при первом обращении
-func (c *Conveyer) getChan(name string) chan string {
-	ch, ok := c.chans[name]
+func (c *Conveyer) Send(name, data string) error {
+	ch, ok := c.inputs[name]
 	if !ok {
-		ch = make(chan string)
-		c.chans[name] = ch
+		return errors.New("channel does not exist")
 	}
-	return ch
-}
-
-func (c *Conveyer) Send(name string, data string) error {
-	ch := c.getChan(name)
 	ch <- data
 	return nil
 }
 
 func (c *Conveyer) Recv(name string) (string, error) {
-	ch, ok := c.chans[name]
+	ch, ok := c.outputs[name]
 	if !ok {
 		return "", errors.New("channel does not exist")
 	}
@@ -72,68 +66,78 @@ func (c *Conveyer) Recv(name string) (string, error) {
 	return data, nil
 }
 
-func (c *Conveyer) RegisterDecorator(name, in, out string, h handlers.DecoratingHandler) {
+func (c *Conveyer) RegisterDecorator(name, inputName, outputName string, h handlers.DecoratingHandler) {
 	c.steps = append(c.steps, step{
-		name: name,
-		tp:   Decorating,
-		in:   in,
-		out:  out,
-		dec:  h,
+		tp:      Decorating,
+		name:    name,
+		input:   inputName,
+		outputs: []string{outputName},
+		dec:     h,
 	})
 }
 
-func (c *Conveyer) RegisterSeparator(name, in, outOk, outErr string, h handlers.SeparatingHandler) {
+func (c *Conveyer) RegisterSeparator(name, inputName string, outputNames []string, h handlers.SeparatingHandler) {
 	c.steps = append(c.steps, step{
-		name:   name,
-		tp:     Separating,
-		in:     in,
-		outOk:  outOk,
-		outErr: outErr,
-		sep:    h,
+		tp:      Separating,
+		name:    name,
+		input:   inputName,
+		outputs: outputNames,
+		sep:     h,
 	})
 }
 
-func (c *Conveyer) RegisterMultiplexer(name, in1, in2, out string, h handlers.MultiplexingHandler) {
+func (c *Conveyer) RegisterMultiplexer(name string, inputNames []string, outputName string, h handlers.MultiplexingHandler) {
 	c.steps = append(c.steps, step{
-		name: name,
-		tp:   Multiplexing,
-		in:   in1 + "," + in2,
-		out:  out,
-		mux:  h,
+		tp:      Multiplexing,
+		name:    name,
+		inputs:  inputNames,
+		outputs: []string{outputName},
+		mux:     h,
 	})
 }
 
 func (c *Conveyer) Run(ctx context.Context) error {
 	c.ctx, c.cancel = context.WithCancel(ctx)
-
+	channels := make(map[string]chan string)
+	for _, s := range c.steps {
+		for _, out := range s.outputs {
+			channels[out] = make(chan string)
+		}
+	}
 	for _, s := range c.steps {
 		switch s.tp {
-
 		case Decorating:
-			in := c.getChan(s.in)
-			out := c.getChan(s.out)
+			in := c.inputs[s.input]
+			if in == nil {
+				in = channels[s.input]
+			}
+			out := channels[s.outputs[0]]
 			go s.dec(c.ctx, in, out)
-
 		case Separating:
-			in := c.getChan(s.in)
-			ok := c.getChan(s.outOk)
-			errCh := c.getChan(s.outErr)
-			go s.sep(c.ctx, in, []chan string{ok, errCh})
-
+			in := c.inputs[s.input]
+			if in == nil {
+				in = channels[s.input]
+			}
+			outs := make([]chan string, len(s.outputs))
+			for i, n := range s.outputs {
+				outs[i] = channels[n]
+			}
+			go s.sep(c.ctx, in, outs)
 		case Multiplexing:
-			parts := []string{}
-			for _, p := range []rune(s.in) {
-				if p != ',' {
-					parts = append(parts, string(p))
+			ins := make([]chan string, len(s.inputs))
+			for i, n := range s.inputs {
+				ch := c.inputs[n]
+				if ch == nil {
+					ch = channels[n]
 				}
+				ins[i] = ch
 			}
-			var inputs []chan string
-			for _, nm := range parts {
-				inputs = append(inputs, c.getChan(nm))
-			}
-			out := c.getChan(s.out)
-			go s.mux(c.ctx, inputs, out)
+			out := channels[s.outputs[0]]
+			go s.mux(c.ctx, ins, out)
 		}
+	}
+	for name, ch := range channels {
+		c.outputs[name] = ch
 	}
 	return nil
 }
@@ -142,7 +146,10 @@ func (c *Conveyer) Stop() {
 	if c.cancel != nil {
 		c.cancel()
 	}
-	for _, ch := range c.chans {
+	for _, ch := range c.inputs {
+		close(ch)
+	}
+	for _, ch := range c.outputs {
 		close(ch)
 	}
 }
