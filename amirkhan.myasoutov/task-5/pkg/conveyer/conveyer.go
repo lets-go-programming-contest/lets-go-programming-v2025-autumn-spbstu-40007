d21f1c654cee470663129def3a65400a60b9d4fd
Amirkhan Myasoutov <>
2025-12-05 23:56:36 +0300

    [TASK-5] fix code

--- amirkhan.myasoutov/task-5/pkg/conveyer/conveyer.go
+++ amirkhan.myasoutov/task-5/pkg/conveyer/conveyer.go
@@ -4,189 +4,129 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"sync"
+
+	"golang.org/x/sync/errgroup"
 )
 
-var (
-	ErrChanNotFound   = errors.New("chan not found")
-	ErrInvalidHandler = errors.New("invalid handler function signature or nil")
-)
+const emptyPayload = "undefined"
 
-type DecoratorHandler func(ctx context.Context, input chan string, output chan string) error
-type MultiplexerHandler func(ctx context.Context, inputs []chan string, output chan string) error
-type SeparatorHandler func(ctx context.Context, input chan string, outputs []chan string) error
+var ErrStreamNotFound = errors.New("stream not found")
 
-type HandlerConfig struct {
-	HandlerFn interface{}
-	InputIDs  []string
-	OutputIDs []string
+type Pipeline struct {
+	bufferSize int
+	streams    map[string]chan string
+	workers    []func(context.Context) error
 }
 
-type Pipeline interface {
-	RegisterDecorator(fn DecoratorHandler, inputID string, outputID string)
-	RegisterMultiplexer(fn MultiplexerHandler, inputsIDs []string, outputID string)
-	RegisterSeparator(fn SeparatorHandler, inputID string, outputsIDs []string)
-	Run(ctx context.Context) error
-	Send(inputID string, data string) error
-	Recv(outputID string) (string, error)
-}
-
-type conveyerImpl struct {
-	channelSize    int
-	channels       map[string]chan string
-	handlerConfigs []HandlerConfig
-	mu             sync.Mutex
-	wg             sync.WaitGroup
-}
-
-func New(size int) Pipeline {
-	return &conveyerImpl{
-		channelSize:    size,
-		channels:       make(map[string]chan string),
-		handlerConfigs: make([]HandlerConfig, 0),
+func New(bufferSize int) *Pipeline {
+	return &Pipeline{
+		bufferSize: bufferSize,
+		streams:    make(map[string]chan string),
+		workers:    make([]func(context.Context) error, 0),
 	}
 }
 
-func (c *conveyerImpl) getChan(id string) chan string {
-
-	if ch, ok := c.channels[id]; ok {
-		return ch
+func (p *Pipeline) makeStream(name string) {
+	if _, ok := p.streams[name]; !ok {
+		p.streams[name] = make(chan string, p.bufferSize)
 	}
-
-	newChan := make(chan string, c.channelSize)
-	c.channels[id] = newChan
-	return newChan
 }
 
-func (c *conveyerImpl) getChannelsByIds(ids []string) []chan string {
-	channels := make([]chan string, len(ids))
-	for i, id := range ids {
-		channels[i] = c.getChan(id)
+func (p *Pipeline) makeStreams(names ...string) {
+	for _, n := range names {
+		p.makeStream(n)
 	}
-	return channels
 }
 
-func (c *conveyerImpl) RegisterDecorator(fn DecoratorHandler, inputID string, outputID string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.handlerConfigs = append(c.handlerConfigs, HandlerConfig{
-		HandlerFn: fn,
-		InputIDs:  []string{inputID},
-		OutputIDs: []string{outputID},
+func (p *Pipeline) RegisterDecorator(
+	handler func(ctx context.Context, source chan string, sink chan string) error,
+	source string,
+	sink string,
+) {
+	p.makeStreams(source)
+	p.makeStreams(sink)
+
+	p.workers = append(p.workers, func(ctx context.Context) error {
+		return handler(ctx, p.streams[source], p.streams[sink])
 	})
-	c.getChan(inputID)
-	c.getChan(outputID)
 }
 
-func (c *conveyerImpl) RegisterMultiplexer(fn MultiplexerHandler, inputsIDs []string, outputID string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.handlerConfigs = append(c.handlerConfigs, HandlerConfig{
-		HandlerFn: fn,
-		InputIDs:  inputsIDs,
-		OutputIDs: []string{outputID},
+func (p *Pipeline) RegisterMultiplexer(
+	handler func(ctx context.Context, sources []chan string, sink chan string) error,
+	sourceNames []string,
+	sink string,
+) {
+	p.makeStreams(sourceNames...)
+	p.makeStreams(sink)
+
+	p.workers = append(p.workers, func(ctx context.Context) error {
+		sourceStreams := make([]chan string, 0, len(sourceNames))
+		for _, name := range sourceNames {
+			sourceStreams = append(sourceStreams, p.streams[name])
+		}
+
+		return handler(ctx, sourceStreams, p.streams[sink])
 	})
-	for _, id := range inputsIDs {
-		c.getChan(id)
-	}
-	c.getChan(outputID)
 }
 
-func (c *conveyerImpl) RegisterSeparator(fn SeparatorHandler, inputID string, outputsIDs []string) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.handlerConfigs = append(c.handlerConfigs, HandlerConfig{
-		HandlerFn: fn,
-		InputIDs:  []string{inputID},
-		OutputIDs: outputsIDs,
+func (p *Pipeline) RegisterSeparator(
+	handler func(ctx context.Context, source chan string, sinks []chan string) error,
+	source string,
+	sinkNames []string,
+) {
+	p.makeStreams(source)
+	p.makeStreams(sinkNames...)
+
+	p.workers = append(p.workers, func(ctx context.Context) error {
+		sinkStreams := make([]chan string, 0, len(sinkNames))
+		for _, name := range sinkNames {
+			sinkStreams = append(sinkStreams, p.streams[name])
+		}
+
+		return handler(ctx, p.streams[source], sinkStreams)
 	})
-	c.getChan(inputID)
-	for _, id := range outputsIDs {
-		c.getChan(id)
-	}
 }
 
-func (c *conveyerImpl) Run(ctx context.Context) error {
-	errChan := make(chan error, len(c.handlerConfigs))
+func (p *Pipeline) Run(ctx context.Context) error {
+	group, groupCtx := errgroup.WithContext(ctx)
 
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
+	for _, handlerFunc := range p.workers {
+		workerFunc := handlerFunc
 
-	for _, config := range c.handlerConfigs {
-		c.wg.Add(1)
-
-		inputChans := c.getChannelsByIds(config.InputIDs)
-		outputChans := c.getChannelsByIds(config.OutputIDs)
-
-		go func(cfg HandlerConfig, ins []chan string, outs []chan string) {
-			defer c.wg.Done()
-			var err error
-
-			switch fn := cfg.HandlerFn.(type) {
-			case DecoratorHandler:
-				if len(ins) != 1 || len(outs) != 1 {
-					err = errors.New("decorator handler requires 1 input and 1 output")
-				} else {
-					err = fn(ctx, ins[0], outs[0])
-				}
-			case MultiplexerHandler:
-				if len(outs) != 1 {
-					err = errors.New("multiplexer handler requires 1 output")
-				} else {
-					err = fn(ctx, ins, outs[0])
-				}
-			case SeparatorHandler:
-				if len(ins) != 1 || len(outs) == 0 {
-					err = errors.New("separator handler requires 1 input and >=1 output")
-				} else {
-					err = fn(ctx, ins[0], outs)
-				}
-			default:
-				err = ErrInvalidHandler
-			}
-
-			if err != nil && err != context.Canceled {
-				errChan <- fmt.Errorf("handler failed: %w", err)
-			}
-		}(config, inputChans, outputChans)
+		group.Go(func() error {
+			return workerFunc(groupCtx)
+		})
 	}
 
-	select {
-	case <-ctx.Done():
-		c.wg.Wait()
-		return ctx.Err()
-	case err := <-errChan:
-		cancel()
-		c.wg.Wait()
-		return err
-	}
-}
-
-func (c *conveyerImpl) Send(inputID string, data string) error {
-	c.mu.Lock()
-	ch, ok := c.channels[inputID]
-	c.mu.Unlock()
-
-	if !ok {
-		return ErrChanNotFound
+	if err := group.Wait(); err != nil {
+		return fmt.Errorf("pipeline workers: %w", err)
 	}
 
-	ch <- data
 	return nil
 }
 
-func (c *conveyerImpl) Recv(outputID string) (string, error) {
-	c.mu.Lock()
-	ch, ok := c.channels[outputID]
-	c.mu.Unlock()
-
+func (p *Pipeline) Send(source string, payload string) error {
+	stream, ok := p.streams[source]
 	if !ok {
-		return "", ErrChanNotFound
+		return ErrStreamNotFound
 	}
 
-	data, open := <-ch
-	if !open {
-		return "undefined", nil
+	stream <- payload
+
+	return nil
+}
+
+func (p *Pipeline) Recv(sink string) (string, error) {
+	stream, ok := p.streams[sink]
+	if !ok {
+		return "", ErrStreamNotFound
 	}
-	return data, nil
+
+	receivedPayload, open := <-stream
+
+	if !open {
+		return emptyPayload, nil
+	}
+
+	return receivedPayload, nil
 }
--- amirkhan.myasoutov/task-5/pkg/handlers/handlers.go
+++ amirkhan.myasoutov/task-5/pkg/handlers/handlers.go
@@ -4,169 +4,115 @@ import (
 	"context"
 	"errors"
 	"strings"
-	"sync"
 )
 
-func safelyCloseChannel(chnl chan string) {
-	defer func() {
-		if r := recover(); r != nil {
-			
-		}
-	}()
-	if chnl != nil {
-		close(chnl)
-	}
-}
+var ErrDecorationSkipped = errors.New("can't be decorated")
 
-func readInputToTransfer(ctx context.Context, input chan string, transfer chan string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case data, ok := <-input:
-			if !ok {
-				return
-			}
+const (
+	skipDecorationKeyword = "no decorator"
+	decorationPrefix      = "decorated: "
+	skipMuxKeyword        = "no multiplexer"
+)
 
-			select {
-			case transfer <- data:
-			case <-ctx.Done():
-				return
-			}
-		}
-	}
-}
-
-func PrefixDecoratorFunc(
-	ctx context.Context,
-	input chan string,
-	output chan string,
-) error {
-	defer safelyCloseChannel(output)
-
-	const prefix = "decorated: "
+func AddPrefixProcessor(ctx context.Context, inStream chan string, outStream chan string) error {
+	defer close(outStream)
 
 	for {
 		select {
 		case <-ctx.Done():
 			return nil
-		case data, ok := <-input:
+
+		case dataChunk, ok := <-inStream:
 			if !ok {
 				return nil
 			}
 
-			if strings.Contains(data, "no decorator") {
-				return errors.New("data error: can't be decorated because it contains 'no decorator'")
+			if strings.Contains(dataChunk, skipDecorationKeyword) {
+				return ErrDecorationSkipped
 			}
 
-			processedData := data
-			if !strings.HasPrefix(data, prefix) {
-				processedData = prefix + data
+			if !strings.HasPrefix(dataChunk, decorationPrefix) {
+				dataChunk = decorationPrefix + dataChunk
 			}
 
 			select {
-			case output <- processedData:
 			case <-ctx.Done():
 				return nil
+			case outStream <- dataChunk:
 			}
 		}
 	}
 }
 
-func SeparatorFunc(
-	ctx context.Context,
-	input chan string,
-	outputs []chan string,
-) error {
-	defer func() {
-		closedChans := make(map[chan string]struct{})
-		for _, output := range outputs {
-			if _, ok := closedChans[output]; !ok {
-				safelyCloseChannel(output)
-				closedChans[output] = struct{}{}
-			}
-		}
-	}()
+func MergeStreamsProcessor(ctx context.Context, inStreams []chan string, outStream chan string) error {
+	defer close(outStream)
 
-	if len(outputs) == 0 {
-		for range input {
-			select {
-			case <-ctx.Done():
-				return nil
-			default:
-			}
-		}
+	if len(inStreams) == 0 {
 		return nil
 	}
 
-	var counter uint64
-	var mu sync.Mutex
-	numOutputs := uint64(len(outputs))
+	for _, sourceStream := range inStreams {
+		localSourceStream := sourceStream
 
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		case data, ok := <-input:
-			if !ok {
-				return nil
+		go func(chLocal chan string) {
+			for {
+				select {
+				case <-ctx.Done():
+					return
+
+				case dataChunk, ok := <-chLocal:
+					if !ok {
+						return
+					}
+
+					if strings.Contains(dataChunk, skipMuxKeyword) {
+						continue
+					}
+
+					select {
+					case <-ctx.Done():
+						return
+					case outStream <- dataChunk:
+					}
+				}
 			}
-
-			mu.Lock()
-			index := counter % numOutputs
-			counter++
-			mu.Unlock()
-
-			output := outputs[index]
-
-			select {
-			case output <- data:
-			case <-ctx.Done():
-				return nil
-			}
-		}
+		}(localSourceStream)
 	}
+
+	<-ctx.Done()
+
+	return nil
 }
 
-func MultiplexerFunc(
-	ctx context.Context,
-	inputs []chan string,
-	output chan string,
-) error {
-	defer safelyCloseChannel(output)
-
-	var wg sync.WaitGroup
-	transfer := make(chan string)
-
-	for _, input := range inputs {
-		wg.Add(1)
-		go readInputToTransfer(ctx, input, transfer, &wg)
-	}
-
-	go func() {
-		wg.Wait()
-		safelyCloseChannel(transfer)
+func RoundRobinSplitter(ctx context.Context, inStream chan string, outStreams []chan string) error {
+	defer func() {
+		for _, outCh := range outStreams {
+			close(outCh)
+		}
 	}()
+
+	sinkCount := len(outStreams)
+	dispatchIndex := 0
 
 	for {
 		select {
 		case <-ctx.Done():
 			return nil
-		case data, ok := <-transfer:
+
+		case dataChunk, ok := <-inStream:
 			if !ok {
 				return nil
 			}
 
-			if strings.Contains(data, "no multiplexer") {
+			if sinkCount == 0 {
 				continue
 			}
 
 			select {
-			case output <- data:
 			case <-ctx.Done():
 				return nil
+			case outStreams[dispatchIndex%sinkCount] <- dataChunk:
+				dispatchIndex++
 			}
 		}
 	}
