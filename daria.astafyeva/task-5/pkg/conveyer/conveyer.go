975aff20dcdac3c3977e5729e6de520861a2b676
Astafyeva Daria <m.astafeva07081985@gmail.com>
2025-12-05 18:11:02 +0300

    [TASK-5] Initial commit

---
+++ daria.astafyeva/task-5/pkg/conveyer/conveyer.go
@@ -0,0 +1,156 @@
+package conveyer
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+)
+
+var ErrChannelMissing = errors.New("channel missing")
+
+type handlerFn func(ctx context.Context) error
+
+type Conveyor struct {
+	bufSize   int
+	chMap     map[string]chan string
+	handlers  []handlerFn
+	syncMutex sync.Mutex
+}
+
+func New(bufSize int) *Conveyor {
+	return &Conveyor{
+		bufSize:   bufSize,
+		chMap:     make(map[string]chan string),
+		handlers:  make([]handlerFn, 0),
+		syncMutex: sync.Mutex{},
+	}
+}
+
+func (c *Conveyor) createChanIfNeeded(id string) chan string {
+	c.syncMutex.Lock()
+	defer c.syncMutex.Unlock()
+	if ch, found := c.chMap[id]; found {
+		return ch
+	}
+	newCh := make(chan string, c.bufSize)
+	c.chMap[id] = newCh
+	return newCh
+}
+
+func (c *Conveyor) RegisterDecorator(
+	decFn func(ctx context.Context, in chan string, out chan string) error,
+	inID string,
+	outID string,
+) {
+	inCh := c.createChanIfNeeded(inID)
+	outCh := c.createChanIfNeeded(outID)
+	c.syncMutex.Lock()
+	c.handlers = append(c.handlers, func(ctx context.Context) error {
+		return decFn(ctx, inCh, outCh)
+	})
+	c.syncMutex.Unlock()
+}
+
+func (c *Conveyor) RegisterMultiplexer(
+	muxFn func(ctx context.Context, ins []chan string, out chan string) error,
+	inIDs []string,
+	outID string,
+) {
+	var inChs []chan string
+	for _, id := range inIDs {
+		inChs = append(inChs, c.createChanIfNeeded(id))
+	}
+	outCh := c.createChanIfNeeded(outID)
+	c.syncMutex.Lock()
+	c.handlers = append(c.handlers, func(ctx context.Context) error {
+		return muxFn(ctx, inChs, outCh)
+	})
+	c.syncMutex.Unlock()
+}
+
+func (c *Conveyor) RegisterSeparator(
+	sepFn func(ctx context.Context, in chan string, outs []chan string) error,
+	inID string,
+	outIDs []string,
+) {
+	inCh := c.createChanIfNeeded(inID)
+	var outChs []chan string
+	for _, id := range outIDs {
+		outChs = append(outChs, c.createChanIfNeeded(id))
+	}
+	c.syncMutex.Lock()
+	c.handlers = append(c.handlers, func(ctx context.Context) error {
+		return sepFn(ctx, inCh, outChs)
+	})
+	c.syncMutex.Unlock()
+}
+
+func (c *Conveyor) Run(ctx context.Context) error {
+	var wg sync.WaitGroup
+	errCh := make(chan error, 1)
+	runCtx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	c.syncMutex.Lock()
+	for _, h := range c.handlers {
+		wg.Add(1)
+		go func(fn handlerFn) {
+			defer wg.Done()
+			if err := fn(runCtx); err != nil {
+				select {
+				case errCh <- err:
+				default:
+				}
+				cancel()
+			}
+		}(h)
+	}
+	c.syncMutex.Unlock()
+
+	go func() {
+		wg.Wait()
+		close(errCh)
+	}()
+
+	select {
+	case err := <-errCh:
+		if err != nil {
+			return fmt.Errorf("conveyor run failed: %w", err)
+		}
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+
+	c.syncMutex.Lock()
+	defer c.syncMutex.Unlock()
+	for _, ch := range c.chMap {
+		close(ch)
+	}
+	return nil
+}
+
+func (c *Conveyor) Send(inID string, value string) error {
+	c.syncMutex.Lock()
+	ch, found := c.chMap[inID]
+	c.syncMutex.Unlock()
+	if !found {
+		return ErrChannelMissing
+	}
+	ch <- value
+	return nil
+}
+
+func (c *Conveyor) Recv(outID string) (string, error) {
+	c.syncMutex.Lock()
+	ch, found := c.chMap[outID]
+	c.syncMutex.Unlock()
+	if !found {
+		return "", ErrChannelMissing
+	}
+	val, ok := <-ch
+	if !ok {
+		return "undefined", nil
+	}
+	return val, nil
+}
---
+++ daria.astafyeva/task-5/pkg/handlers/handlers.go
@@ -0,0 +1,97 @@
+package handlers
+
+import (
+	"context"
+	"errors"
+	"strings"
+	"sync"
+)
+
+var (
+	ErrCannotDecorate   = errors.New("can't be decorated")
+	ErrNoOutputChannels = errors.New("no output channels")
+)
+
+const (
+	skipMux     = "no multiplexer"
+	skipDec     = "no decorator"
+	decPrefix   = "decorated: "
+)
+
+func PrefixDecoratorFunc(ctx context.Context, inCh chan string, outCh chan string) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, open := <-inCh:
+			if !open {
+				return nil
+			}
+			if strings.Contains(data, skipDec) {
+				return ErrCannotDecorate
+			}
+			if !strings.HasPrefix(data, decPrefix) {
+				data = decPrefix + data
+			}
+			select {
+			case outCh <- data:
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	}
+}
+
+func MultiplexerFunc(ctx context.Context, inChs []chan string, outCh chan string) error {
+	var muxWg sync.WaitGroup
+	for _, in := range inChs {
+		muxWg.Add(1)
+		go func(ch <-chan string) {
+			defer muxWg.Done()
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case data, open := <-ch:
+					if !open {
+						return
+					}
+					if strings.Contains(data, skipMux) {
+						continue
+					}
+					select {
+					case outCh <- data:
+					case <-ctx.Done():
+						return
+					}
+				}
+			}
+		}(in)
+	}
+	muxWg.Wait()
+	return nil
+}
+
+func SeparatorFunc(ctx context.Context, inCh chan string, outChs []chan string) error {
+	if len(outChs) == 0 {
+		return ErrNoOutputChannels
+	}
+	var pos int
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case data, open := <-inCh:
+			if !open {
+				return nil
+			}
+			target := outChs[pos%len(outChs)]
+			select {
+			case target <- data:
+				pos++
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	}
+}
