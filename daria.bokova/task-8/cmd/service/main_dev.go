86f1b2d6792a495bb7e2063b905f26d674803641
Bokova Daria <bokovad2@gmail.com>
2025-12-20 01:19:02 +0300

    [TASK-8] fix commit 13

---
+++ daria.bokova/task-8/cmd/service/main_dev.go
@@ -0,0 +1,37 @@
+//go:build dev
+
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+)
+
+//go:embed dev.yaml
+var configData string
+
+func main() {
+	lines := strings.Split(configData, "\n")
+	env, level := "", ""
+
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if strings.HasPrefix(line, "environment:") {
+			parts := strings.SplitN(line, ":", 2)
+			if len(parts) == 2 {
+				env = strings.TrimSpace(parts[1])
+				env = strings.Trim(env, "\"")
+			}
+		}
+		if strings.HasPrefix(line, "log_level:") {
+			parts := strings.SplitN(line, ":", 2)
+			if len(parts) == 2 {
+				level = strings.TrimSpace(parts[1])
+				level = strings.Trim(level, "\"")
+			}
+		}
+	}
+
+	fmt.Printf("%s %s\n", env, level)
+}
