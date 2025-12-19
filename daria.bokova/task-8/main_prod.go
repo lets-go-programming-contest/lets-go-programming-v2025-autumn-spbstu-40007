fabb6c12068b16f5e0db3536dd869246fb0a74e8
Bokova Daria <bokovad2@gmail.com>
2025-12-20 01:11:06 +0300

    [TASK-8] fix commit 12

---
+++ daria.bokova/task-8/main_prod.go
@@ -0,0 +1,24 @@
+//go:build !dev
+
+package main
+
+import (
+	_ "embed"
+	"fmt"
+
+	"gopkg.in/yaml.v3"
+)
+
+//go:embed prod.yaml
+var configData []byte
+
+type Config struct {
+	Environment string `yaml:"environment"`
+	LogLevel    string `yaml:"log_level"`
+}
+
+func main() {
+	var cfg Config
+	yaml.Unmarshal(configData, &cfg)
+	fmt.Printf("%s %s\n", cfg.Environment, cfg.LogLevel)
+}
