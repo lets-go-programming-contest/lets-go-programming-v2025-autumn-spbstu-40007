bf9ff64e6264edfaf211659b3de797fe915633d4
UwUshkin <ekaterinanazimova07@gmail.com>
2025-12-22 21:02:40 +0300

    [TASK-8] Add code

--- ekaterina.nazimova/task-8/cmd/service/main.go
+++ ekaterina.nazimova/task-8/cmd/service/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"github.com/UwUshkin/task-8/pkg/config"
 )
 
 func main() {
-
+	cfg := config.GetConfig()
+	fmt.Printf("%s %s\n", cfg.Environment, cfg.LogLevel)
 }
