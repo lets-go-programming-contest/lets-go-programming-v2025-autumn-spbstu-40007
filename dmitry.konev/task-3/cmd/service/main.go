7d693ca2ab0e07d6eb062e97eb9348e9b5e461ff
Konev Dmitri <konevdmitrij791@gmail.com>
2026-01-01 20:34:28 +0300

    [TASK-3] 20

--- dmitry.konev/task-3/cmd/service/main.go
+++ dmitry.konev/task-3/cmd/service/main.go
@@ -41,12 +41,17 @@ type ResultCurrency struct {
 func main() {
 	configPath := flag.String("config", "", "path to config file")
 	flag.Parse()
+
 	if *configPath == "" {
 		panic("config flag is required")
 	}
+
 	config := loadConfig(*configPath)
+
 	valutes := loadXML(config.InputFile)
+
 	result := transform(valutes)
+
 	saveJSON(config.OutputFile, result)
 }
 
@@ -55,13 +60,17 @@ func loadConfig(path string) Config {
 	if err != nil {
 		panic(err)
 	}
+
 	var cfg Config
+
 	if err := yaml.Unmarshal(data, &cfg); err != nil {
 		panic(err)
 	}
+
 	if cfg.InputFile == "" || cfg.OutputFile == "" {
 		panic("config fields must not be empty")
 	}
+
 	return cfg
 }
 
@@ -70,53 +79,81 @@ func loadXML(path string) []Valute {
 	if err != nil {
 		panic(err)
 	}
-	defer func() { _ = file.Close() }()
+
+	defer func() {
+		_ = file.Close()
+	}()
 
 	decoder := xml.NewDecoder(file)
+
 	decoder.CharsetReader = func(charset string, input io.Reader) (io.Reader, error) {
 		if strings.EqualFold(charset, "windows-1251") {
 			return charmap.Windows1251.NewDecoder().Reader(input), nil
 		}
+
 		return input, nil
 	}
+
 	var curs ValCurs
+
 	if err := decoder.Decode(&curs); err != nil {
 		panic(err)
 	}
+
 	return curs.Valutes
 }
 
 func transform(valutes []Valute) []ResultCurrency {
 	result := make([]ResultCurrency, 0, len(valutes))
+
 	for _, valute := range valutes {
 		value, err := parseFloat(valute.Value)
 		if err != nil {
 			panic(err)
 		}
-		result = append(result, ResultCurrency{valute.NumCode, valute.CharCode, value})
+
+		result = append(result, ResultCurrency{
+			NumCode:  valute.NumCode,
+			CharCode: valute.CharCode,
+			Value:    value,
+		})
 	}
-	sort.Slice(result, func(i, j int) bool { return result[i].Value > result[j].Value })
+
+	sort.Slice(result, func(i, j int) bool {
+		return result[i].Value > result[j].Value
+	})
+
 	return result
 }
 
 func saveJSON(path string, data []ResultCurrency) {
 	dir := filepath.Dir(path)
+
 	if err := os.MkdirAll(dir, dirPerm); err != nil {
 		panic(err)
 	}
+
 	file, err := os.Create(path)
 	if err != nil {
 		panic(err)
 	}
-	defer func() { _ = file.Close() }()
+
+	defer func() {
+		_ = file.Close()
+	}()
 
 	encoder := json.NewEncoder(file)
 	encoder.SetIndent("", "  ")
+
 	if err := encoder.Encode(data); err != nil {
 		panic(err)
 	}
 }
 
 func parseFloat(s string) (float64, error) {
-	return strconv.ParseFloat(strings.ReplaceAll(s, ",", "."), 64)
+	value, err := strconv.ParseFloat(strings.ReplaceAll(s, ",", "."), 64)
+	if err != nil {
+		return 0, fmt.Errorf("parse float: %w", err)
+	}
+	return value, nil
 }
