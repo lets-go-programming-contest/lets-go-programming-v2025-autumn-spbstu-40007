ce9d4d30b756626f89a7cfbe115caa21323da109
Konev Dmitri <konevdmitrij791@gmail.com>
2026-01-02 18:37:34 +0300

    [TASK-2-1] 1

---
+++ dmitry.konev/task-2-1/cmd/service/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	minTemp = 15
+	maxTemp = 30
+)
+
+func processDepartment(scanner *bufio.Scanner, numEmployees int) {
+	currentMin := minTemp
+	currentMax := maxTemp
+
+	for i := 0; i < numEmployees; i++ {
+		if !scanner.Scan() {
+			return
+		}
+
+		line := strings.TrimSpace(scanner.Text())
+		parts := strings.Split(line, " ")
+		if len(parts) != 2 {
+			fmt.Println(-1)
+			continue
+		}
+
+		operator := parts[0]
+		value, err := strconv.Atoi(parts[1])
+		if err != nil {
+			fmt.Println(-1)
+			continue
+		}
+
+		switch operator {
+		case ">=":
+			if value > currentMin {
+				currentMin = value
+			}
+		case "<=":
+			if value < currentMax {
+				currentMax = value
+			}
+		default:
+			fmt.Println(-1)
+			continue
+		}
+
+		if currentMin <= currentMax {
+			fmt.Println(currentMin)
+		} else {
+			fmt.Println(-1)
+		}
+	}
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	if !scanner.Scan() {
+		return
+	}
+
+	numDepartments, err := strconv.Atoi(strings.TrimSpace(scanner.Text()))
+	if err != nil || numDepartments < 1 || numDepartments > 1000 {
+		return
+	}
+
+	for d := 0; d < numDepartments; d++ {
+		if !scanner.Scan() {
+			return
+		}
+
+		numEmployees, err := strconv.Atoi(strings.TrimSpace(scanner.Text()))
+		if err != nil || numEmployees < 1 || numEmployees > 1000 {
+			return
+		}
+
+		processDepartment(scanner, numEmployees)
+	}
+}
