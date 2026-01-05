ea6f6a48a9f886c0de65ee9fd993f54adbde0af4
Timothée <sainetsauf@disroot.org>
2026-01-05 16:04:40 +0300

    [TASK-6] (#195)
    * [TASK-6] Initial commit.
    
    * [TASK-6] ...
    
    * [TASK-6] Linter is rubbish.
    
    * [TASK-6] ...
    
    * [TASK-6] Ultraviolence is over?
    
    * [TASK-6] Ultraviolence is over?
    
    * [TASK-6] Ultraviolence is over?

---
+++ artyom.grinko/task-6/internal/db/db_test.go
@@ -0,0 +1,232 @@
+package db_test
+
+//nolint:gofumpt
+import (
+	"database/sql"
+	"errors"
+	"testing"
+
+	"task-6/internal/db"
+	"task-6/internal/functionals"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/require"
+)
+
+var errSomeError = errors.New("some erreur")
+
+var singersNames = []string{ //nolint:gochecknoglobals
+	"Édith Piaf",
+	"Françoise Hardy",
+	"Mylène Farmer",
+	"Édith Piaf",
+	"Joe Dassin",
+}
+
+func TestGetNames(t *testing.T) {
+	t.Parallel()
+
+	t.Run("queryFailure", func(t *testing.T) {
+		t.Parallel()
+
+		database, mock, err := sqlmock.New()
+
+		require.NoError(t, err)
+
+		defer database.Close()
+
+		mock.
+			ExpectQuery("SELECT name FROM users").
+			WillReturnError(sql.ErrNoRows)
+
+		databaseService := db.New(database)
+		names, err := databaseService.GetNames()
+		require.Empty(t, names)
+		require.Error(t, err)
+	})
+
+	t.Run("scanFailure", func(t *testing.T) {
+		t.Parallel()
+
+		database, mock, err := sqlmock.New()
+
+		require.NoError(t, err)
+
+		defer database.Close()
+
+		rows := mock.
+			NewRows([]string{"name"}).
+			AddRow(nil)
+
+		mock.
+			ExpectQuery("SELECT name FROM users").
+			WillReturnRows(rows).
+			WillReturnError(nil)
+
+		databaseService := db.New(database)
+		names, err := databaseService.GetNames()
+		require.Empty(t, names)
+		require.Error(t, err)
+	})
+
+	t.Run("rowsFailure", func(t *testing.T) {
+		t.Parallel()
+
+		database, mock, err := sqlmock.New()
+
+		require.NoError(t, err)
+
+		defer database.Close()
+
+		rows := mock.
+			NewRows([]string{"name"})
+
+		functionals.Iter(func(name string) {
+			rows.AddRow(name)
+		}, singersNames)
+
+		rows.RowError(0, errSomeError)
+
+		mock.
+			ExpectQuery("SELECT name FROM users").
+			WillReturnRows(rows)
+
+		databaseService := db.New(database)
+		names, err := databaseService.GetNames()
+		require.Empty(t, names)
+		require.Error(t, err)
+	})
+
+	t.Run("querySuccess", func(t *testing.T) {
+		t.Parallel()
+
+		database, mock, err := sqlmock.New()
+
+		require.NoError(t, err)
+
+		defer database.Close()
+
+		rows := mock.
+			NewRows([]string{"name"})
+
+		functionals.Iter(func(name string) {
+			rows.AddRow(name)
+		}, singersNames)
+
+		mock.
+			ExpectQuery("SELECT name FROM users").
+			WillReturnRows(rows)
+
+		databaseService := db.New(database)
+		names, err := databaseService.GetNames()
+		require.Equal(t, names, singersNames)
+		require.NoError(t, err)
+	})
+}
+
+func TestUniqueNames(t *testing.T) {
+	t.Parallel()
+
+	t.Run("queryFailure", func(t *testing.T) {
+		t.Parallel()
+
+		database, mock, err := sqlmock.New()
+
+		require.NoError(t, err)
+
+		defer database.Close()
+
+		mock.
+			ExpectQuery("SELECT DISTINCT name FROM users").
+			WillReturnError(sql.ErrNoRows)
+
+		databaseService := db.New(database)
+		names, err := databaseService.GetUniqueNames()
+		require.Empty(t, names)
+		require.Error(t, err)
+	})
+
+	t.Run("scanFailure", func(t *testing.T) {
+		t.Parallel()
+
+		database, mock, err := sqlmock.New()
+
+		require.NoError(t, err)
+
+		defer database.Close()
+
+		rows := mock.
+			NewRows([]string{"name"}).
+			AddRow(nil)
+
+		mock.
+			ExpectQuery("SELECT DISTINCT name FROM users").
+			WillReturnRows(rows).
+			WillReturnError(nil)
+
+		databaseService := db.New(database)
+		names, err := databaseService.GetUniqueNames()
+		require.Empty(t, names)
+		require.Error(t, err)
+	})
+
+	t.Run("rowsFailure", func(t *testing.T) {
+		t.Parallel()
+
+		database, mock, err := sqlmock.New()
+
+		require.NoError(t, err)
+
+		defer database.Close()
+
+		rows := mock.
+			NewRows([]string{"name"})
+
+		functionals.Iter(func(name string) {
+			rows.AddRow(name)
+		}, singersNames)
+
+		rows.RowError(0, errSomeError)
+
+		mock.
+			ExpectQuery("SELECT DISTINCT name FROM users").
+			WillReturnRows(rows)
+
+		databaseService := db.New(database)
+		names, err := databaseService.GetUniqueNames()
+		require.Empty(t, names)
+		require.Error(t, err)
+	})
+
+	t.Run("querySuccess", func(t *testing.T) {
+		t.Parallel()
+
+		database, mock, err := sqlmock.New()
+
+		require.NoError(t, err)
+
+		defer database.Close()
+
+		rows := mock.
+			NewRows([]string{"name"})
+
+		functionals.Iter(func(name string) {
+			rows.AddRow(name)
+		}, singersNames)
+
+		expectedRows := sqlmock.NewRows([]string{"name"})
+
+		functionals.Iter(func(name string) {
+			expectedRows.AddRow(name)
+		}, functionals.Unique(singersNames))
+
+		mock.
+			ExpectQuery("SELECT DISTINCT name FROM users").
+			WillReturnRows(expectedRows)
+
+		databaseService := db.New(database)
+		names, err := databaseService.GetUniqueNames()
+		require.Equal(t, names, functionals.Unique(singersNames))
+		require.NoError(t, err)
+	})
+}
