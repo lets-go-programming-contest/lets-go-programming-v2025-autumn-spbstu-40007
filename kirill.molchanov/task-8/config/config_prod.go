8eb5ec500e1602857cc2a43470efddd359a3a57f
kirill <118661700+kuri1o4ka@users.noreply.github.com>
2026-01-06 18:26:06 +0300

    [TASK-8] (#205)
    * [TASK-8]
    
    * [TASK-8] fix trouble

---
+++ kirill.molchanov/task-8/config/config_prod.go
@@ -0,0 +1,19 @@
+//go:build !dev
+
+package config
+
+import (
+	_ "embed"
+
+	"gopkg.in/yaml.v3"
+)
+
+//go:embed prod.yaml
+var prodData []byte
+
+func load() Config {
+	var cfg Config
+	_ = yaml.Unmarshal(prodData, &cfg)
+
+	return cfg
+}
