eb1a2009bdc69ba2fd76e3147ff123e92b576c3a
Гусев Илья <ilya007gs@yandex.ru>
2026-01-08 01:53:11 +0300

    [TASK-2-1] Reduce cyclomatic complexity

--- ilya.gusev/task-2-1/cmd/service/main.go
+++ ilya.gusev/task-2-1/cmd/service/main.go
@@ -15,49 +15,56 @@ func main() {
 	}
 
 	for range officeCount {
-		var workersCount int
+		processOffice()
+	}
+}
 
-		_, err = fmt.Scanln(&workersCount)
-		if err != nil || workersCount < 1 || workersCount > 1000 {
-			fmt.Println(-1)
+func processOffice() {
+	var workersCount int
 
+	_, err := fmt.Scanln(&workersCount)
+	if err != nil || workersCount < 1 || workersCount > 1000 {
+		fmt.Println(-1)
+
+		return
+	}
+
+	lowerBound := 15
+	upperBound := 30
+
+	for range workersCount {
+		lowerBound, upperBound = processWorker(lowerBound, upperBound)
+		if lowerBound == -1 {
 			return
 		}
-
-		lowerBound := 15
-		upperBound := 30
-
-		for range workersCount {
-			var operator string
-
-			var temp int
-
-			_, err = fmt.Scanln(&operator, &temp)
-			if err != nil || (operator != ">=" && operator != "<=") {
-				fmt.Println(-1)
-
-				return
-			}
-
-			if temp < 15 || temp > 30 {
-				fmt.Println(-1)
-
-				return
-			}
-
-			if operator == ">=" && temp > lowerBound {
-				lowerBound = temp
-			}
-
-			if operator == "<=" && temp < upperBound {
-				upperBound = temp
-			}
-
-			if lowerBound > upperBound {
-				fmt.Println(-1)
-			} else {
-				fmt.Println(lowerBound)
-			}
-		}
 	}
+}
+
+func processWorker(lowerBound, upperBound int) (int, int) {
+	var operator string
+
+	var temp int
+
+	_, err := fmt.Scanln(&operator, &temp)
+	if err != nil || (operator != ">=" && operator != "<=") || temp < 15 || temp > 30 {
+		fmt.Println(-1)
+
+		return -1, -1
+	}
+
+	if operator == ">=" && temp > lowerBound {
+		lowerBound = temp
+	}
+
+	if operator == "<=" && temp < upperBound {
+		upperBound = temp
+	}
+
+	if lowerBound > upperBound {
+		fmt.Println(-1)
+	} else {
+		fmt.Println(lowerBound)
+	}
+
+	return lowerBound, upperBound
 }
