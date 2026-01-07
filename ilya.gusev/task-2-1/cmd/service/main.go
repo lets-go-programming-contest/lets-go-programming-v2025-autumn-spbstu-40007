f3eb5c2d3baf560d6d1e66fb45d3c1bd49091256
Гусев Илья <ilya007gs@yandex.ru>
2026-01-08 01:27:53 +0300

    [TASK-2-1]

---
+++ ilya.gusev/task-2-1/cmd/service/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var officeCount int
+
+	_, err := fmt.Scanln(&officeCount)
+	if err != nil || officeCount < 1 || officeCount > 1000 {
+		fmt.Println(-1)
+		return
+	}
+
+	for i := 0; i < officeCount; i++ {
+		var workersCount int
+		_, err = fmt.Scanln(&workersCount)
+		if err != nil || workersCount < 1 || workersCount > 1000 {
+			fmt.Println(-1)
+			return
+		}
+
+		lowerBound := 15
+		upperBound := 30
+
+		for j := 0; j < workersCount; j++ {
+			var operator string
+			var temp int
+
+			_, err = fmt.Scanln(&operator, &temp)
+			if err != nil || (operator != ">=" && operator != "<=") {
+				fmt.Println(-1)
+				return
+			}
+
+			if temp < 15 || temp > 30 {
+				fmt.Println(-1)
+				return
+			}
+
+			if operator == ">=" {
+				if temp > lowerBound {
+					lowerBound = temp
+				}
+			} else {
+				if temp < upperBound {
+					upperBound = temp
+				}
+			}
+
+			if lowerBound > upperBound {
+				fmt.Println(-1)
+			} else {
+				fmt.Println(lowerBound)
+			}
+		}
+	}
+}
