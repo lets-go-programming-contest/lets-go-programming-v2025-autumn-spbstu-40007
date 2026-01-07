12a27bc4085db28b7c913eb36b7a1f3c2af99e2f
Гусев Илья <ilya007gs@yandex.ru>
2026-01-08 01:35:55 +0300

    [TASK-2-1] Fix linter errors

--- ilya.gusev/task-2-1/cmd/service/main.go
+++ ilya.gusev/task-2-1/cmd/service/main.go
@@ -10,32 +10,37 @@ func main() {
 	_, err := fmt.Scanln(&officeCount)
 	if err != nil || officeCount < 1 || officeCount > 1000 {
 		fmt.Println(-1)
+
 		return
 	}
 
-	for i := 0; i < officeCount; i++ {
+	for i := range officeCount {
 		var workersCount int
+
 		_, err = fmt.Scanln(&workersCount)
 		if err != nil || workersCount < 1 || workersCount > 1000 {
 			fmt.Println(-1)
+
 			return
 		}
 
 		lowerBound := 15
 		upperBound := 30
 
-		for j := 0; j < workersCount; j++ {
+		for j := range workersCount {
 			var operator string
 			var temp int
 
 			_, err = fmt.Scanln(&operator, &temp)
 			if err != nil || (operator != ">=" && operator != "<=") {
 				fmt.Println(-1)
+
 				return
 			}
 
 			if temp < 15 || temp > 30 {
 				fmt.Println(-1)
+
 				return
 			}
 
