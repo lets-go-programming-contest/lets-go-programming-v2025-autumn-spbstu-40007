f024180860fef91698919de1c64f7d0dfe9b825f
mart22052006 <157504417+Mart22052006@users.noreply.github.com>
2026-01-08 21:08:17 +0300

    [TASK-8] fix6

--- mihail.artemyev/task-8/pkg/config/config.go
+++ mihail.artemyev/task-8/pkg/config/config.go
@@ -1,22 +1,22 @@
 package config
 
 import (
-    "fmt"
+	"fmt"
 
-    "gopkg.in/yaml.v3"
+	"gopkg.in/yaml.v3"
 )
 
 type Configuration struct {
-    Environment string `yaml:"environment"`
-    LogLevel    string `yaml:"log_level"`
+	Environment string `yaml:"environment"`
+	LogLevel    string `yaml:"log_level"`
 }
 
 func Load() (Configuration, error) {
-    var configuration Configuration
+	var configuration Configuration
 
-    if err := yaml.Unmarshal(configBytes, &configuration); err != nil {
-        return Configuration{}, fmt.Errorf("parse configuration: %w", err)
-    }
+	if err := yaml.Unmarshal(configBytes, &configuration); err != nil {
+		return Configuration{}, fmt.Errorf("parse configuration: %w", err)
+	}
 
-    return configuration, nil
+	return configuration, nil
 }
