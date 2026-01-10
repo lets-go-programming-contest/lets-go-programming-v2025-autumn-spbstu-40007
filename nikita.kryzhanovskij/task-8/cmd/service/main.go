db435edaf90e4e4f8cb73281d3fc0d743df2fd72
Omifes <russ.out.338@gmail.com>
2026-01-11 02:44:22 +0300

    [TASK-8] (#226)
    * [TASK-8] initial commit
    
    * [TASK-8]
    
    * [TASK-8]
    
    * [TASK-8]
    
    * [TASK-8]
    
    * [TASK-8]
    
    * [TASK-8]
    
    * [TASK-8]
    
    * [TASK-8]
    
    * [TASK-8]

---
+++ nikita.kryzhanovskij/task-8/cmd/service/main.go
@@ -0,0 +1,43 @@
+//nolint:gofumpt
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"task-8/pkg/config"
+)
+
+const maxParts = 2
+
+func main() {
+	configData := config.GetConfig()
+	lines := strings.Split(configData, "\n")
+
+	var env, level string
+
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		if line == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		parts := strings.SplitN(line, ":", maxParts)
+		if len(parts) != maxParts {
+			continue
+		}
+
+		key := strings.TrimSpace(parts[0])
+		val := strings.TrimSpace(parts[1])
+
+		if key == "environment" {
+			env = val
+		} else if key == "log_level" {
+			level = val
+		}
+	}
+
+	if env != "" && level != "" {
+		fmt.Printf("%s %s", env, level)
+	}
+}
